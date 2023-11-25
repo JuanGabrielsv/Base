376b97f6bee8633429992c1435410911952e3c9a
JuanGabrielsv <juangabrielsv@gmail.com>
2023-11-25 20:48:15 +0100

    T2_ejercicios_practicos_repaso6

---
+++ Espacios de trabajo eclipse/Workspace 1/Tema2/src/t2_ejercicios_practicos_repaso/T2_ejercicios_practicos_repaso7.java
@@ -0,0 +1,10 @@
+package t2_ejercicios_practicos_repaso;
+
+public class T2_ejercicios_practicos_repaso7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
