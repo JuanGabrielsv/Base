bb8e40af71adee94dbcac0845f6c1a99590225e5
JuanGabrielsv <juangabrielsv@gmail.com>
2023-11-27 13:57:55 +0100

    Haciendo el T2_ejercicios_practicos_repaso11

---
+++ Espacios de trabajo eclipse/Workspace 1/Tema2/src/t2_ejercicios_practicos_repaso/T2_ejercicios_practicos_repaso11.java
@@ -0,0 +1,68 @@
+/* EJERCICIO 11
+Realizar un programa que solicite dos números enteros al usuario. Luego mostrar el siguiente
+menú:
+*** MENÚ ***
+1. Sumar
+2. Restar
+3. Multiplicar
+4. Dividir
+0. Salir
+Hacer con los dos números la operación elegida (el usuario indicará 1, 2, 3 o 4) y mostrar el
+resultado. Luego volver a mostrar el menú. Si el usuario escoge la opción 0 (Salir), el programa
+termina.
+OJO: Si el segundo número es un cero, no se podrán dividir. En ese caso, si elige la opción 4,
+tendremos que indicar que la división no es posible. */
+package t2_ejercicios_practicos_repaso;
+
+import java.util.Scanner;
+
+public class T2_ejercicios_practicos_repaso11 {
+	
+	public static final Scanner SC = new Scanner(System.in);
+	
+	public static void main (String[]args) {
+		
+		Integer entradaUsuarioOpcion = 0;
+		Integer entradaUsuario1 = 0;
+		Integer entradaUsuario2 = 0;
+		Boolean check = true;
+		Integer calculo = 0;
+		
+		do {
+			
+			System.out.println("*** MENU ***");
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("0. Salir");
+			
+			System.out.println("Elige la opción: ");
+			entradaUsuarioOpcion = SC.nextInt();
+			
+			switch (entradaUsuarioOpcion) {
+			
+			case 0:
+				check = false;
+				break;
+			case 1:
+				System.out.println("Has elegido SUMAR: ");
+				System.out.println("Introduce el primer número (entero): ");
+				entradaUsuario1 = SC.nextInt();
+				System.out.println("Introduce el segundo número (entero): ");
+				entradaUsuario2 = SC.nextInt();
+				
+				calculo = (entradaUsuario1 + entradaUsuario2);
+				
+				System.out.println(calculo);
+				break;
+			
+			default:
+				
+			}
+			
+		} while (check);		
+		
+	}
+
+}
