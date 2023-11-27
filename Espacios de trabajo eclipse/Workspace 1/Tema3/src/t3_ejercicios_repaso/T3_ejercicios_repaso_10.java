ee1a660429c178dde166c3de3a7a4297d46fd692
JuanGabrielsv <juangabrielsv@gmail.com>
2023-11-27 17:54:26 +0100

    Haciendo el T3_ejercicios_repaso10

---
+++ Espacios de trabajo eclipse/Workspace 1/Tema3/src/t3_ejercicios_repaso/T3_ejercicios_repaso_10.java
@@ -0,0 +1,54 @@
+/* EJERCICIO 10
+Haz un programa que pueda calcular la nota media de los alumnos de clase. Para ello, en primer
+lugar, el programa debe preguntar al usuario cuántos alumnos hay. A continuación, para cada
+alumno, solicitará su nombre y las notas de los 3 exámenes realizados. Después, una vez que
+haya preguntado todo, deberá mostrar un listado con las notas medias de cada alumno y la nota
+media de la clase.
+Ejemplo de ejecución:
+¿Cuántos alumnos hay en clase?
+2
+Cuál es el nombre el alumno 1
+Laura
+Cuál es la nota de Laura para el examen 1?
+5
+Cuál es la nota de Laura para el examen 2?
+9
+Cuál es la nota de Laura para el examen 3?
+7
+Cuál es el nombre el alumno 2
+Antonio
+Cuál es la nota de Antonio para el examen 1?
+2
+Cuál es la nota de Antonio para el examen 2?
+4
+Cuál es la nota de Antonio para el examen 3?
+9
+LISTADO DE NOTAS
+ Alumno: Laura - Nota media: 7
+ Alumno: Antonio - Nota media: 5
+Media de los alumnos: 6 */
+package t3_ejercicios_repaso;
+
+import java.util.Scanner;
+
+public class T3_ejercicios_repaso_10 {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Scanner SC = new Scanner(System.in);
+		
+		String nombreAlumno1 = "";
+		Integer entradaNumAlumnos = 0;
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("Cuántos alumnos hay");
+			entradaNumAlumnos
+			
+		}		
+
+
+	}
+
+}
