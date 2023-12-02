88a288863662c3510af4866cea72970a734aa55f
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-02 10:54:27 +0100

    Añadido ejercicio T2_ejercicio_practicos_repaso

--- Espacios de trabajo eclipse/Workspace 1/Tema2/src/t2_ejercicios_practicos_repaso/T2_ejercicios_practicos_repaso11.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema2/src/t2_ejercicios_practicos_repaso/T2_ejercicios_practicos_repaso11.java
@@ -17,31 +17,33 @@ package t2_ejercicios_practicos_repaso;
 import java.util.Scanner;
 
 public class T2_ejercicios_practicos_repaso11 {
-	
+
 	public static final Scanner SC = new Scanner(System.in);
-	
-	public static void main (String[]args) {
-		
+
+	public static void main(String[] args) {
+
 		Integer entradaUsuarioOpcion = 0;
 		Integer entradaUsuario1 = 0;
 		Integer entradaUsuario2 = 0;
+		double calculoDouble = 0;
+		double usuario1 = 0;
+		double usuario2 = 0;
 		Boolean check = true;
 		Integer calculo = 0;
-		
+
 		do {
-			
+
 			System.out.println("*** MENU ***");
 			System.out.println("1. Sumar");
 			System.out.println("2. Restar");
 			System.out.println("3. Multiplicar");
 			System.out.println("4. Dividir");
 			System.out.println("0. Salir");
-			
 			System.out.println("Elige la opción: ");
 			entradaUsuarioOpcion = SC.nextInt();
-			
+
 			switch (entradaUsuarioOpcion) {
-			
+
 			case 0:
 				check = false;
 				break;
@@ -51,9 +53,8 @@ public class T2_ejercicios_practicos_repaso11 {
 				entradaUsuario1 = SC.nextInt();
 				System.out.println("Introduce el segundo número (entero): ");
 				entradaUsuario2 = SC.nextInt();
-				
 				calculo = (entradaUsuario1 + entradaUsuario2);
-				
+
 				System.out.println(calculo);
 				break;
 			case 2:
@@ -62,14 +63,50 @@ public class T2_ejercicios_practicos_repaso11 {
 				entradaUsuario1 = SC.nextInt();
 				System.out.println("Introduce el segundo número (entero): ");
 				entradaUsuario2 = SC.nextInt();
-				
+
 				calculo = (entradaUsuario1 - entradaUsuario2);
+
+				System.out.println(calculo);
+				break;
+			case 3:
+				System.out.println("Has elegido MULTIPLICAR: ");
+				System.out.println("Introduce el primer número (entero): ");
+				entradaUsuario1 = SC.nextInt();
+				System.out.println("Introduce el segundo número (entero): ");
+				entradaUsuario2 = SC.nextInt();
+
+				calculo = (entradaUsuario1 * entradaUsuario2);
+
+				System.out.println(calculo);
+				break;
+			case 4:
+				System.out.println("Has elegido DIVIDIR: ");
+				System.out.println("Introduce el primer número (entero): ");
+				entradaUsuario1 = SC.nextInt();
+				System.out.println("Introduce el segundo número (entero): ");
+				entradaUsuario2 = SC.nextInt();
+
+				if (entradaUsuario1 <= 0 || entradaUsuario2 <= 0) {
+
+					System.out.println("ERROR - Ninguno de los dos número puede ser 0, vuelva a intentarlo.");
+
+				} else {
+					usuario1 = entradaUsuario1.doubleValue();
+					usuario2 = entradaUsuario2.doubleValue();
+					calculoDouble = calculo.doubleValue();
+
+					calculoDouble = (usuario1 / usuario2);
+
+					System.out.println(calculoDouble);
+					break;
+				}
+
 			default:
-				
+
 			}
-			
-		} while (check);		
-		
+
+		} while (check);
+
 	}
 
 }
