3d37b58b23c95bfbce98c21e986cff1bf0110c0b
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-02 18:10:15 +0100

    Añadido T2_ejercicios_practicos_repaso18

--- Espacios de trabajo eclipse/Workspace 1/Tema2/src/t2_ejercicios_practicos_repaso/T2_ejercicios_practicos_repaso17.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema2/src/t2_ejercicios_practicos_repaso/T2_ejercicios_practicos_repaso17.java
@@ -13,48 +13,31 @@ import java.util.Scanner;
 public class T2_ejercicios_practicos_repaso17 {
 
 	public static void main(String[] args) {
-		
-		/*Scanner sc = new Scanner(System.in);		
-		Integer entradaUsuario = 0;
-		Integer suma = 0;
-		
-		do {
-			System.out.println("Introduce un número entero: ");
-			entradaUsuario = sc.nextInt();
-		}
-		while(entradaUsuario <= 2);		
-		
-		for (int i = 0; i <= entradaUsuario; i++) {
-			System.out.print((suma = entradaUsuario + suma) + " ");						
-		}*/
-		
+
 		Scanner scanner = new Scanner(System.in);
 		Integer numeroSerie = 0;
 		Integer anterior = 0;
 		Integer ultimo = 1;
 		Integer nuevoValor;
-		
+
 		do {
 			System.out.println("Dame un nmero mayor a 2");
 			numeroSerie = scanner.nextInt();
-		}
-		while(numeroSerie <= 2);
-		System.out.print("La serie de fibonacci del nmero "+ numeroSerie + " es: ");
+		} while (numeroSerie <= 2);
+
+		System.out.print("La serie de fibonacci del numero " + numeroSerie + " es: ");
 		System.out.print(anterior + ", " + ultimo);
-		for(int serie=2;serie<numeroSerie;serie++)
-		{
+
+		for (int serie = 2; serie < numeroSerie; serie++) {
 			nuevoValor = anterior + ultimo;
 			anterior = ultimo;
 			ultimo = nuevoValor;
-			System.out.print( ", " + nuevoValor);
-			
+			System.out.print(", " + nuevoValor);
+
 		}
-		
+
 		scanner.close();
 
-		
-		
-		
 	}
 
 }
