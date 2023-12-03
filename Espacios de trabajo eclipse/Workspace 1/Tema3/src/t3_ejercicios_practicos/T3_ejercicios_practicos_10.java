f14f1997acaa952427f2e34921ddceadd898670a
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-04 00:00:06 +0100

    Añadido sin terminar T3_ejercicios_practicos10

---
+++ Espacios de trabajo eclipse/Workspace 1/Tema3/src/t3_ejercicios_practicos/T3_ejercicios_practicos_10.java
@@ -0,0 +1,19 @@
+/* EJERCICIO 10
+Crea un array de un tamaño que sea indicado por el usuario (lo solicitaremos primero). El
+tamaño tendrá que ser mayor a 2.
+A continuación, llena el array con la serie de Fibonacci. Las dos primeras posiciones del array
+las puedes llenar a mano con un 0 y un 1 respectivamente. El resto de posiciones hasta
+completar el tamaño del array, las debes llenar con un bucle que vaya calculando el valor de la
+posición i a partir del valor de las posiciones anteriores.
+Al terminar, imprime el contenido del array para mostrarlo al usuario. 
+*/
+package t3_ejercicios_practicos;
+
+public class T3_ejercicios_practicos_10 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
