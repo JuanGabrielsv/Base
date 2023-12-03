005875ee1106e2c3bb12cf671fa4f1a2ac6a10a2
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-03 21:46:22 +0100

    Añadido T3_ejercicios_practicos_6

--- Espacios de trabajo eclipse/Workspace 1/Tema3/src/t3_ejercicios_practicos/T3_ejercicios_practicos_6.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema3/src/t3_ejercicios_practicos/T3_ejercicios_practicos_6.java
@@ -9,19 +9,27 @@ import java.util.Scanner;
 public class T3_ejercicios_practicos_6 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		String entradaUsuario = "";
-		String[] array1;
-		String[] array2;
-		String[] array3;
-		
+		String[] array1 = new String[1];
+		String[] array2 = new String[1];
+		String[] array3 = new String[1];
+
 		System.out.println("Tines que introducir 3 palabras: ");
 		System.out.println("Primera palabra: ");
-		array1 = new String[sc.nextLine()];
-		
-		
+		array1[0] = sc.nextLine();
 
+		System.out.println("Segunda palabra: ");
+		array2[0] = sc.nextLine();
+
+		System.out.println("Tercera palabra: ");
+		array3[0] = sc.nextLine();
+
+		for (int i = 0; i < array3.length; i++) {
+			System.out.println(array1[i] + array2[i] + array3[i]);
+
+		}
 
 	}
 
