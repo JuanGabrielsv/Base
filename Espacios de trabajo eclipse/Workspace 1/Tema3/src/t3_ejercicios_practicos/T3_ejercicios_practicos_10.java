e34af5388999f8ef4b03ceae3b68f87584108dcc
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-04 11:17:48 +0100

    Añadidos T3_ejercicios_practicos10 y 11

--- Espacios de trabajo eclipse/Workspace 1/Tema3/src/t3_ejercicios_practicos/T3_ejercicios_practicos_10.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema3/src/t3_ejercicios_practicos/T3_ejercicios_practicos_10.java
@@ -9,10 +9,39 @@ Al terminar, imprime el contenido del array para mostrarlo al usuario.
 */
 package t3_ejercicios_practicos;
 
+import java.util.Scanner;
+
 public class T3_ejercicios_practicos_10 {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+
+		// Pedir el tamao de manera continua hasta que no sea menor a 2
+		Integer tam = null;
+		do {
+			System.out.println("Dame el tamao de la serie");
+			tam = scanner.nextInt();
+		} while (tam < 2);
+
+		// Declaramos array e inicializamos las 2 primeras casillas
+		Integer[] fibonacci = new Integer[tam];
+		fibonacci[0] = 0;
+		fibonacci[1] = 1;
+
+		// Recorrer el array y llenar el resto de posiciones5.
+
+		for (int i = 2; i < fibonacci.length; i++) {
+			fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		}
+
+		// Imprimo el contenido del array
+		for (int i = 0; i < fibonacci.length; i++) {
+			System.out.print(fibonacci[i] + " ");
+		}
+
+		// Cierro scanner
+		scanner.close();
 
 	}
 
