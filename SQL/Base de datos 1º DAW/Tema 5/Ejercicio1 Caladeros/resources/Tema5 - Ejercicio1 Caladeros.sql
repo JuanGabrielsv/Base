2a7445c56f5da75c6b6f6e39878389bf2f50cc14
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-07 20:31:58 +0100

    .

--- SQL/Base de datos 1º DAW/Tema 5/Ejercicio1 Caladeros/resources/Tema5 - Ejercicio1 Caladeros.sql
+++ SQL/Base de datos 1º DAW/Tema 5/Ejercicio1 Caladeros/resources/Tema5 - Ejercicio1 Caladeros.sql
@@ -1,7 +1,7 @@
 // CREAMOS LAS TABLAS
 
 CREATE TABLE barcos (
-    matricula_barcos VARCHAR2(100) PRIMARY KEY,
+    matricula_barco VARCHAR2(100) PRIMARY KEY,
     nombre VARCHAR2(100),
     clase VARCHAR2(100),
     armador VARCHAR2(100),
@@ -10,37 +10,53 @@ CREATE TABLE barcos (
     );
     
 CREATE TABLE lotes (
-    codigo_lotes INT PRIMARY KEY,
-    matricula_barcos VARCHAR2(100),
+    codigo_lote INT PRIMARY KEY,
+    matricula_barco VARCHAR2(100),
     num_kg NUMBER(9,2),
     precio_kg_salida NUMBER(9,2),
     precio_kg_vendido NUMBER(9,2),
     fecha_venta DATE,
-    cod_especie INT
+    codigo_especie INT
     );
     
 CREATE TABLE especies (
-    codigo_especies INT PRIMARY KEY,
-    nombre_especies VARCHAR2(100),
+    codigo_especie INT PRIMARY KEY,
+    nombre_especie VARCHAR2(100),
     tipo VARCHAR2(100),
     cupo_barco NUMBER(9,2),
     caladero_ppal VARCHAR2(100)
     );
 
 CREATE TABLE caladeros (
-    codigo_caladeros INT PRIMARY KEY,
-    nombre_caladeros VARCHAR2(100),
+    codigo_caladero INT PRIMARY KEY,
+    nombre_caladero VARCHAR2(100),
     ubicacion VARCHAR2(100),
     especie_ppal VARCHAR2(100)
     );
     
 CREATE TABLE fechas_capturas (
-    codigo_especies INT,
-    codigo_caladeros INT,
+    codigo_especie INT,
+    codigo_caladero INT,
     fecha_inicio DATE,
     fecha_fin DATE,
-    PRIMARY KEY (codigo_especies, codigo_caladeros)
+    PRIMARY KEY (codigo_especie, codigo_caladero)
     );
     
 // FUNCIONAL HASTA AQUÍ.
+
+ALTER TABLE lotes
+    ADD FOREIGN KEY (matricula_barco)
+    REFERENCES barcos(matricula_barco) ON DELETE CASCADE;
+
+ALTER TABLE lotes 
+    ADD FOREIGN KEY (codigo_especie)
+    REFERENCES especies(codigo_especie) ON DELETE CASCADE;
+    
+ALTER TABLE especies 
+    ADD FOREIGN KEY (caladero_ppal)
+    REFERENCES caladeros(codigo_caladero) 
+
+
+    
+
     
