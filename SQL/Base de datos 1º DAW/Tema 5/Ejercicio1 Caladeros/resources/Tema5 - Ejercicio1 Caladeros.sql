c9c7d588f88439e5f58f4b66b3abaddba26436a1
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-07 23:56:17 +0100

    .

--- SQL/Base de datos 1º DAW/Tema 5/Ejercicio1 Caladeros/resources/Tema5 - Ejercicio1 Caladeros.sql
+++ SQL/Base de datos 1º DAW/Tema 5/Ejercicio1 Caladeros/resources/Tema5 - Ejercicio1 Caladeros.sql
@@ -1,3 +1,8 @@
+DROP TABLE barcos CASCADE CONSTRAINTS;
+DROP TABLE caladeros CASCADE CONSTRAINTS;
+DROP TABLE especies CASCADE CONSTRAINTS;
+DROP TABLE fechas_capturas CASCADE CONSTRAINTS;
+DROP TABLE lotes CASCADE CONSTRAINTS;
 // CREAMOS LAS TABLAS
 
 CREATE TABLE barcos (
@@ -42,8 +47,6 @@ CREATE TABLE fechas_capturas (
     PRIMARY KEY (codigo_especie, codigo_caladero)
     );
     
-// FUNCIONAL HASTA AQUÍ.
-
 ALTER TABLE lotes
     ADD FOREIGN KEY (matricula_barco)
     REFERENCES barcos(matricula_barco) ON DELETE CASCADE;
@@ -52,11 +55,48 @@ ALTER TABLE lotes
     ADD FOREIGN KEY (codigo_especie)
     REFERENCES especies(codigo_especie) ON DELETE CASCADE;
     
+ALTER TABLE especies
+    MODIFY caladero_ppal INT;
+    
 ALTER TABLE especies 
     ADD FOREIGN KEY (caladero_ppal)
-    REFERENCES caladeros(codigo_caladero) 
+    REFERENCES caladeros(codigo_caladero); 
 
+ALTER TABLE caladeros
+    MODIFY especie_ppal INT;
 
+ALTER TABLE caladeros
+    ADD FOREIGN KEY (especie_ppal)
+    REFERENCES especies(codigo_especie) ON DELETE SET NULL;
     
+ALTER TABLE fechas_capturas
+    ADD FOREIGN KEY (codigo_especie)
+    REFERENCES especies(codigo_especie);
+    
+ALTER TABLE fechas_capturas
+    ADD FOREIGN KEY (codigo_caladero)
+    REFERENCES caladeros(codigo_caladero);
 
+ALTER TABLE barcos
+    MODIFY matricula_barco VARCHAR2(7);
+    
+ALTER TABLE barcos
+    ADD CHECK (REGEXP_LIKE(matricula_barco, '[A-Z][A-Z]-[0-9][0-9][0-9][0-9]'));
+    
+ALTER TABLE lotes
+    MODIFY fecha_venta NOT NULL;
+    
+ALTER TABLE lotes
+    ADD CHECK (precio_kg_vendido > precio_kg_salida);
+    
+ALTER TABLE lotes
+    ADD CHECK (num_kg > 0);
+    
+ALTER TABLE lotes
+    ADD CHECK (precio_kg_vendido > 0);
+    
+ALTER TABLE lotes
+    ADD CHECK (precio_kg_salida > 0);
+    
+ // FUNCIONAL HASTA AQUÍ.
     
