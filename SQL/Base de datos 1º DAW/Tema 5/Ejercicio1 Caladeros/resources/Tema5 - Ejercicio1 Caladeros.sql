9f8b0e2184edc01daddc00450c521c158daa821a
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-07 14:07:39 +0100

    .

--- SQL/Base de datos 1º DAW/Tema 5/Ejercicio1 Caladeros/resources/Tema5 - Ejercicio1 Caladeros.sql
+++ SQL/Base de datos 1º DAW/Tema 5/Ejercicio1 Caladeros/resources/Tema5 - Ejercicio1 Caladeros.sql
@@ -1,5 +1,5 @@
 CREATE TABLE barcos (
-    matricula VARCHAR2(100) PRIMARY KEY,
+    matricula_barcos VARCHAR2(100) PRIMARY KEY,
     nombre VARCHAR2(100),
     clase VARCHAR2(100),
     armador VARCHAR2(100),
@@ -9,4 +9,23 @@ CREATE TABLE barcos (
     
 CREATE TABLE lotes (
     codigo_lotes INT PRIMARY KEY,
-    matricula VARCHAR2(100),
+    matricula_barcos VARCHAR2(100),
+    num_kg NUMBER(9,2),
+    precio_kg_salida NUMBER(9,2),
+    precio_kg_vendido NUMBER(9,2),
+    fecha_venta DATE,
+    cod_especie INT
+    );
+    
+CREATE TABLE especies (
+    codigo_especies INT PRIMARY KEY,
+    nombre_especies VARCHAR2(100),
+    tipo VARCHAR2(100),
+    cupo_barco NUMBER(9,2),
+    caladero_ppal VARCHAR2(100)
+    );
+    
+// FUNCIONAL HASTA AQUÍ.
+
+
+    
