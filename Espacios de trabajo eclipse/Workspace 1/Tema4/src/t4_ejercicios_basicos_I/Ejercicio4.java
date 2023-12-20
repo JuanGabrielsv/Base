bd7f1ec25ff174c8b261f30e604d29d8ded8e7be
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-20 15:23:22 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/Tema4/src/t4_ejercicios_basicos_I/Ejercicio4.java
@@ -0,0 +1,32 @@
+/* EJERCICIO 4
+Repite el ejercicio anterior, pero reutilizando (llamando a) los métodos creados en el Ejercicio 1 y 2. */
+
+package t4_ejercicios_basicos_I;
+
+import java.util.Random;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();		
+		Integer num = random.nextInt(1,3);
+		
+		String resultado = getMayMin(num, "hola");
+		System.out.println(resultado);
+		
+	}
+
+	public static String getMayMin(Integer num, String cadena) {
+		
+		if (num == 1) {
+			cadena = cadena.trim().toUpperCase();
+		}
+		else {
+			cadena = cadena.trim().toLowerCase();
+		}		
+		return cadena;
+
+	}
+
+}
