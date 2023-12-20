8850571dde11cc6f789beb5104c9d0d11748eb1e
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-20 15:53:15 +0100

    .

--- Espacios de trabajo eclipse/Workspace 1/Tema4/src/t4_ejercicios_basicos_I/Ejercicio4.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema4/src/t4_ejercicios_basicos_I/Ejercicio4.java
@@ -3,30 +3,28 @@ Repite el ejercicio anterior, pero reutilizando (llamando a) los métodos creado
 
 package t4_ejercicios_basicos_I;
 
-import java.util.Random; 
+import java.util.Random;
 
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		
-		Random random = new Random();		
-		Integer num = random.nextInt(1,3);
-		
+
+		Random random = new Random();
+		Integer num = random.nextInt(1, 3);
+
 		String resultado = getMayMin(num, "hola");
 		System.out.println(resultado);
-		
+
 	}
 
 	public static String getMayMin(Integer num, String cadena) {
-		
+
 		if (num == 1) {
 			cadena = Ejercicio1.getMayusculas(cadena);
-			
-		}
-		else {
+		} else {
 			cadena = Ejercicio2.getMinusculas(cadena);
-			
-		}		
+		}
+
 		return cadena;
 	}
 
--- Espacios de trabajo eclipse/Workspace 1/Tema4/src/t4_ejercicios_basicos_I/Ejercicio5.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema4/src/t4_ejercicios_basicos_I/Ejercicio5.java
@@ -1,3 +1,7 @@
+/* EJERCICIO 5
+Crea un método llamado sumar que reciba dos números enteros y devuelva el resultado de la suma. Pruébalo
+desde el método main con algún test. */
+
 package t4_ejercicios_basicos_I;
 
 public class Ejercicio5 {
