72187418f08a0e0504076e41432044ea93170683
JuanGabrielsv <juangabrielsv@gmail.com>
2023-12-21 12:32:40 +0100

    .

--- SQL/Base de datos 1º DAW/Tema 5/Ejercicio3 Fútbol/resources/Tema 5 - Ejercicio 3 Fútbol.sql
+++ SQL/Base de datos 1º DAW/Tema 5/Ejercicio3 Fútbol/resources/Tema 5 - Ejercicio 3 Fútbol.sql
@@ -3,7 +3,7 @@ DROP TABLE equipos CASCADE CONSTRAINTS;
 DROP TABLE partidos CASCADE CONSTRAINTS;
 
 CREATE TABLE futbolistas (
-    id_futbolista INT,
+    id_futbolista VARCHAR2(50),
     nombre_futbolista VARCHAR2(100),
     apellidos VARCHAR2(100),
     fecha_nacimiento DATE,
@@ -23,7 +23,7 @@ CREATE TABLE equipos (
 CREATE TABLE partidos (
     id_partido INT,
     arbitro VARCHAR2(100),
-    resultado INT CHECK (REGEXP_LIKE(resultado,'[99-99]')),
+    resultado VARCHAR2(100) CHECK (REGEXP_LIKE(resultado,'[99]-[99]')),
     estadio VARCHAR2(100),
     fecha_partido DATE,
     PRIMARY KEY(id_partido)
@@ -51,9 +51,28 @@ ALTER TABLE futbolistas
 ALTER TABLE futbolistas
     ADD CHECK (fecha_nacimiento > DATE '1978-01-01');
     
-INSERT INTO equipos VALUES (1,'sevilla','ramon sanchez pizjuan',500000000);
-INSERT INTO equipos VALUES (2,'betis','benito villamarin',550000000);
-INSERT INTO equipos VALUES (3,'barcelona','camp nou',800000000);
+INSERT INTO equipos VALUES (1,'Sevilla','Ramon Sanchez Pizjuan',500000000);
+INSERT INTO equipos VALUES (2,'Betis','Benito Villamarin',550000000);
+INSERT INTO equipos VALUES (3,'Barcelona','Camp Nou',800000000);
+
+INSERT INTO futbolistas VALUES ('F00123','Sergio','Rico',DATE'1992-05-23','portero',60000,1);
+INSERT INTO futbolistas VALUES ('F00223','Mariano','Díaz',DATE'1993-06-15','delantero',55000,1);
+INSERT INTO futbolistas VALUES ('F00323','Ivan','Rakitic',DATE'1989-02-08','mediocentro',75000,1);
+INSERT INTO futbolistas VALUES ('F00423','Claudio','Bravo',DATE'1991-01-18','portero',55000,2);
+INSERT INTO futbolistas VALUES ('F00523','Guido','Rodriguez',DATE'1990-12-12','mediocentro',51000,2);
+INSERT INTO futbolistas VALUES ('F00623','Assane','Diao',DATE'1989-06-05','delantero',88000,2);
+INSERT INTO futbolistas VALUES ('F00723','Ander','Astralaga',DATE'1995-08-05','portero',51000,3);
+INSERT INTO futbolistas VALUES ('F00823','Iñigo','Martinez',DATE'1996-07-02','defensa',62000,3);
+INSERT INTO futbolistas VALUES ('F00923','Ferran','Torres',DATE'1997-05-26','delantero',58000,3);
+
+//INSERT INTO partidos VALUES (1,'Ricardo de Burgos','3-0','Ramon Sanchez Pizjuan',DATE'2023-03-12',1);
+
+
+
+
+
+
+
 
 
     
