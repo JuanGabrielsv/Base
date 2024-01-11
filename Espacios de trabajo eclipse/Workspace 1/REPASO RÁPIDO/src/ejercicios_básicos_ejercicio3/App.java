e4d23fcc06b90618173a96c9a80153ccd55bc2e6
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-11 13:36:44 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio3/App.java
@@ -0,0 +1,19 @@
+/*
+EJERCICIO 3
+Crea un método llamado getMayMin que reciba dos parámetros:
+ Un número que podrá ser 1 o 2
+ Una cadena
+Tendrá que coger la cadena recibida y quitarle los espacios en blanco del principio y del final.
+Luego, si el número recibido es 1, devolverá esa cadena en mayúsculas. Si el número recibido
+es 2, devolverá esa cadena en minúsculas.
+Pruébalo desde el método main con algún test. 
+*/
+package ejercicios_básicos_ejercicio3;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
