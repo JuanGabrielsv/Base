64bfa89318340182b6801c026f8e3ae98ac4af2c
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-11 23:11:27 +0100

    .

--- Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio3/App.java
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio3/App.java
@@ -15,11 +15,37 @@ import java.util.Random;
 public class App {
 
 	public static void main(String[] args) {
-		
-
-	}
 	
-	public static String getMayMin (Integer numero, String cadena) {
-		return 
+//FORMA 1: Imprimimos directamente la cadena pasándole el método.
+/*		
+		Random random = new Random();
+		Integer rd = random.nextInt(1,3);		
+		
+		System.out.println(rd);
+		System.out.println(getMayMin(rd, " ESTO es UNA prueba "));
+*/
+		
+//FORMA 2: Pasamos el resultado del método a una variable y después la imprimimos.
+/*		
+		Random random = new Random();
+		Integer rd = random.nextInt(1,3);
+		String resultado = null;
+		
+		System.out.println(rd);
+		resultado = getMayMin(rd, " ESTO es UNA prueba ");
+		System.out.println(resultado);
+*/
+	}
+
+// Creamos el método getMayMin fuera de la clase main.
+	
+	public static String getMayMin (Integer num, String cadena) {
+		if (num == 1) {
+			cadena = cadena.trim().toUpperCase();
+		}
+		else if (num == 2) {
+			cadena = cadena.trim().toLowerCase();
+		}		
+		return cadena;
 	}
 }
