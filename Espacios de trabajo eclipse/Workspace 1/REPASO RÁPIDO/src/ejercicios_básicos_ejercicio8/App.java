3525c2620832d9bf2a516d961e5fae1802d7bce6
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-12 12:46:32 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio8/App.java
@@ -0,0 +1,18 @@
+/*
+EJERCICIO 8
+Crea un nuevo método en la clase “Calculadora” que sea calcular. Recibirá tres parámetros:
+- Una cadena que será “ADD”, “SUB”, “MUL” o “DIV”
+- Dos números enteros
+En función de la cadena recibida, se tendrá que sumar, restar, multiplicar o dividir los números
+recibidos. Reutiliza los métodos ya creados en los ejercicios anteriores.
+Declara las cadenas “ADD”, “SUB”, “MUL” o “DIV” como constantes.
+Prueba el método nuevo con varias pruebas desde el main (usa las constantes).    
+*/
+package ejercicios_básicos_ejercicio8;
+
+public class App {
+
+	public static void main(String[] args) {
+			
+	}	
+}
