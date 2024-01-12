f5b36615a0e7b43b0f5b7dbd8153c3587cfc2b78
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-12 11:57:28 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio7/App.java
@@ -0,0 +1,14 @@
+/*
+EJERCICIO 7
+Crea un nuevo método en la clase “Calculadora” que sea restar, otro multiplicar y otro dividir.
+Todos reciben por parámetro dos enteros y deben devolver un entero con el resultado de la
+operación.   
+*/
+package ejercicios_básicos_ejercicio7;
+
+public class App {
+
+	public static void main(String[] args) {		
+		
+	}	
+}
---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio7/Calculadora.java
@@ -0,0 +1,11 @@
+/*
+EJERCICIO 7
+Crea un nuevo método en la clase “Calculadora” que sea restar, otro multiplicar y otro dividir.
+Todos reciben por parámetro dos enteros y deben devolver un entero con el resultado de la
+operación.   
+*/
+package ejercicios_básicos_ejercicio7;
+
+public class Calculadora {
+	
+}
