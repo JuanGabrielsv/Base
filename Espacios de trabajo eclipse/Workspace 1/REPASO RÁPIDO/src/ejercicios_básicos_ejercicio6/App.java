ae21dd70d491567bf38985da164359d8d0274413
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-12 11:55:22 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio6/App.java
@@ -0,0 +1,24 @@
+/*
+EJERCICIO 6
+Lleva el método realizado en el ejercicio anterior a una nueva clase llamada “Calculadora”.
+Prueba su funcionamiento desde el método main con algún test.   
+*/
+package ejercicios_básicos_ejercicio6;
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Integer num1 = null;
+		Integer num2 = null;
+		
+		System.out.println("Dame un número: ");
+		num1 = sc.nextInt();
+		System.out.println("Dame otro número: ");
+		num2 = sc.nextInt();
+		System.out.println(Calculadora.sumar(num1, num2));
+		sc.close();
+	}	
+}
