2865ec2d4c3ba28358791e83c56f782bfd1635d1
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-15 12:43:27 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio9/App.java
@@ -0,0 +1,14 @@
+/*
+EJERCICIO 9
+Crea un método llamado imprimirArray en una clase llamada ArrayUtils que reciba un array de
+cadenas y lo imprima por completo.
+Pruébalo desde el método main con algún test.     
+*/
+package ejercicios_básicos_ejercicio9;
+
+public class App {	
+	
+	public static void main(String[] args) {
+		
+	}	
+}
