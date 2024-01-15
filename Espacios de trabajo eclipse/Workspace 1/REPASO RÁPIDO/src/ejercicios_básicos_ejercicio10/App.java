608c589d83477e8a394ed87b1662ad95b8ed0837
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-15 16:40:11 +0100

    Añadido comienzo ejercicio10 Tema 4

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio10/App.java
@@ -0,0 +1,15 @@
+/*
+EJERCICIO 10
+En la clase ArrayUtils, crea un método llamado obtenerPalabra que reciba un array de cadenas
+y un número N por parámetros. Debe devolver la cadena que está en la posición N del array. Si
+N no es una posición válida del array, tendrá que devolver una cadena vacía.
+Pruébalo desde el método main con algún test.      
+*/
+package ejercicios_básicos_ejercicio10;
+
+public class App {	
+	
+	public static void main(String[] args) {		
+		
+	}	
+}
