fd83421bd74cd748a22b8e77ddefc832fe111139
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-17 12:27:08 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio18/App.java
@@ -0,0 +1,16 @@
+/*
+EJERCICIO 18
+Crea una clase llamada Alumno que tenga como atributos privados el dni, nombre, edad y
+nota. Además, tendrá métodos get y set para todos los atributos. Luego crea un programa que
+lea por consola los datos de un alumno y los registre en un objeto de la clase que acabas de
+crear.       
+*/
+package ejercicios_básicos_ejercicio18;
+
+public class App {	
+	
+	public static void main(String[] args) {
+		
+
+	}	
+}
