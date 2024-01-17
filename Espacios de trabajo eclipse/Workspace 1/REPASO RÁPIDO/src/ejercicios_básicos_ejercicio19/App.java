a224f6a380bb4410e7243f513bbc5c81cdbdae80
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-17 16:05:14 +0100

    .

---
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio19/App.java
@@ -0,0 +1,36 @@
+/*
+EJERCICIO 18
+Crea una clase llamada Alumno que tenga como atributos privados el dni, nombre, edad y
+nota. Además, tendrá métodos get y set para todos los atributos. Luego crea un programa que
+lea por consola los datos de un alumno y los registre en un objeto de la clase que acabas de
+crear.       
+*/
+package ejercicios_básicos_ejercicio19;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Alumno alumno = new Alumno();
+
+		System.out.println("1/4 - Dime el nombre del Alumno: ");
+		alumno.setNombre(sc.nextLine());
+		System.out.println("2/4 - Introduce su DNI: ");
+		alumno.setDni(sc.nextLine());
+		System.out.println("3/4 - ¿Cuántos años tiene?");
+		alumno.setEdad(sc.nextInt());
+		System.out.println("4/4 - Dime la nota:");
+		alumno.setNota(sc.nextDouble());
+		sc.close();
+
+		System.out.println("\nLos datos introducidos son los siguientes: \n ");
+		System.out.println("NOMBRE: " + alumno.getNombre());
+		System.out.println("DNI: " + alumno.getDni());
+		System.out.println("EDAD: " + alumno.getEdad());
+		System.out.println("NOTA: " + alumno.getNota());
+
+	}
+}
