a8fe5f5e0e3a85eb46c4f14708c15fa4f17fdfea
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-17 16:04:24 +0100

    .

--- Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio18/Alumno.java
+++ Espacios de trabajo eclipse/Workspace 1/REPASO RÁPIDO/src/ejercicios_básicos_ejercicio18/Alumno.java
@@ -1,46 +1,49 @@
-/* EJERCICIO 18
+/* 
+EJERCICIO 18
 Crea una clase llamada Alumno que tenga como atributos privados el dni, nombre, edad y
 nota. Además, tendrá métodos get y set para todos los atributos. Luego crea un programa que
 lea por consola los datos de un alumno y los registre en un objeto de la clase que acabas de
-crear. */
+crear. 
+*/
 package ejercicios_básicos_ejercicio18;
 
 public class Alumno {
-	
+
 	private String dni;
 	private String nombre;
-	private Integer edad;	
+	private Integer edad;
 	private Double nota;
-	
-	/*public Alumno() {
-		dni = "";
-		nombre = "";
-		edad = 0;
-		nota = 0.0;
-	}*/
+
 	public String getDni() {
 		return dni;
 	}
+
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public Integer getEdad() {
 		return edad;
 	}
+
 	public void setEdad(Integer edad) {
 		this.edad = edad;
 	}
+
 	public Double getNota() {
 		return nota;
 	}
+
 	public void setNota(Double nota) {
 		this.nota = nota;
 	}
-	
+
 }
