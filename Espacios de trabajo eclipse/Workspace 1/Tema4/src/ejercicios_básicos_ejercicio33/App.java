98439d543a7dd85f26da084e2eaf9e13a22ba5cc
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-26 16:40:41 +0100

    .

--- Espacios de trabajo eclipse/Workspace 1/Tema4/src/ejercicios_básicos_ejercicio33/App.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema4/src/ejercicios_básicos_ejercicio33/App.java
@@ -1,3 +1,34 @@
+/*
+EJERCICIO 33
+Construye una nueva clase Semaforo que debe funcionar del siguiente modo:
+
+1) Tendrá dos atributos:
+a. color, de tipo String. Podrá ser Rojo, Ámbar o Verde. Lo correcto es que
+declares estos valores como constantes.
+b. parpadeando, de tipo Boolean.
+
+2) Sólo tendrá el constructor por defecto, que tendrá que inicializar el color en Rojo sin
+parpadear.
+
+3) Crea los métodos get y set
+
+4) En los métodos set, debes validar lo siguiente:
+a. Cuando se intente cambiar el color, si se indica un color no válido, entonces no
+se modificará el atributo.
+b. Cuando se intente cambiar el estado de parpadeo, si se intenta activar el
+parpadeo cuando el color es distinto a Ámbar, entonces no se modificará.
+
+5) Sobrescribe el método toString() para que devuelva lo siguiente en función del valor de
+los atributos:
+a. Semáforo en ROJO
+b. Semáforo en VERDE
+c. Semáforo en ÁMBAR
+d. Semáforo en ÁMBAR parpadeando
+
+6) Añade un método cambiarEstado() que modifique el valor de los atributos del
+semáforo siguiendo este ciclo de estados:
+VERDE -> ÁMBAR (parpadeando) -> ÁMBAR (sin parpadear) -> ROJO -> VERDE y así 
+*/
 package ejercicios_básicos_ejercicio33;
 
 public class App {
--- Espacios de trabajo eclipse/Workspace 1/Tema4/src/ejercicios_básicos_ejercicio35/App.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema4/src/ejercicios_básicos_ejercicio35/App.java
@@ -1,14 +1,8 @@
 /*
-EJERCICIO 25
-Crea método equals() en la clase Alumno para definir que un alumno será igual a otro si tienen
-el mismo DNI.
-Desde tu programa, crea un array de 3 Alumnos. Todos los alumnos pertenecen al curso con
-identificador = 1 y descripción “DAM-DAW”. El resto de datos de cada alumno, debes
-solicitarlos al usuario.
-Cuando tengas el array completo, comprueba si hay alumnos repetidos dentro del array (ten
-en cuenta que dos alumnos estarán repetidos en función de lo que se haya implementado en
-el método equals). Si hay alumnos repetidos, muestra un mensaje de error al usuario.
+EJERCICIO 35
+Vuele a hacer el ejercicio 25 utilizando List en lugar de arrays.
 */
+
 package ejercicios_básicos_ejercicio35;
 
 import java.util.Scanner;
