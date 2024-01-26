b2a50b7df7e98e41ccfc7833e9beb5921eebf484
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-26 16:38:10 +0100

    .

--- Espacios de trabajo eclipse/Workspace 1/Tema4/src/ejercicios_básicos_ejercicio34/App.java
+++ Espacios de trabajo eclipse/Workspace 1/Tema4/src/ejercicios_básicos_ejercicio34/App.java
@@ -17,60 +17,57 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		List<String> listaCadenas = new ArrayList<>();
-		
+
 		for (int i = 0; i < 5; i++) {
 			System.out.println("Introduce la cadena para la posición " + i + " de la lista");
-			listaCadenas.add(sc.nextLine());			
+			listaCadenas.add(sc.nextLine());
 		}
-		
-		sc.close();	
-		
+
+		sc.close();
+
 //FORMAS DE IMPRIMIR UNA LISTA
-		
+
 		for (int i = 0; i < listaCadenas.size(); i++) {
-			System.out.println(listaCadenas.get(i));			
+			System.out.println(listaCadenas.get(i));
 		}
-		
+
 		System.out.println(listaCadenas);
-		
+
 		System.out.println(listaCadenas.toString());
-		
+
 //1. Pon todas las cadenas de la lista en mayúsculas. Imprime la lista de nuevo.
-		
+
 		for (int i = 0; i < listaCadenas.size(); i++) {
 			listaCadenas.set(i, listaCadenas.get(i).toUpperCase());
 		}
 
 //IMPRIMIR LA LISTA CON LOS ELEMENTOS EN MAYÚSCULA
-		
+
 		System.out.println(listaCadenas);
-		
+
 //2. Comprueba si la lista contiene alguna cadena vacía. En tal caso, escribe un mensaje por consola indicándolo. (No lo hagas con un bucle)
-		
+
 		if (listaCadenas.contains("")) {
 			System.out.println("La lista tiene espacios vacios");
 		} else {
 			System.out.println("La lista no contiene espacios vacios");
 		}
-		
+
 //3. Borra todas las cadenas de la lista que tengan una longitud menor a 6 caracteres.
-		
+
 		Iterator<String> iterator = listaCadenas.iterator();
 		while (iterator.hasNext()) {
-			
+
 			if (iterator.next().length() < 6) {
 				iterator.remove();
-			}			
+			}
 		}
-		
-		System.out.println(listaCadenas); 
-		
-			
+
+		System.out.println(listaCadenas);
+
 	}
-		
+
 }
-
-
---
+++ Espacios de trabajo eclipse/Workspace 1/Tema4/src/ejercicios_básicos_ejercicio35/Curso.java
@@ -0,0 +1,42 @@
+/*
+EJERCICIO 25
+Crea método equals() en la clase Alumno para definir que un alumno será igual a otro si tienen
+el mismo DNI.
+Desde tu programa, crea un array de 3 Alumnos. Todos los alumnos pertenecen al curso con
+identificador = 1 y descripción “DAM-DAW”. El resto de datos de cada alumno, debes
+solicitarlos al usuario.
+Cuando tengas el array completo, comprueba si hay alumnos repetidos dentro del array (ten
+en cuenta que dos alumnos estarán repetidos en función de lo que se haya implementado en
+el método equals). Si hay alumnos repetidos, muestra un mensaje de error al usuario.
+*/
+package ejercicios_básicos_ejercicio35;
+
+public class Curso {
+
+	private Integer identificador;
+	private String descripcion;
+	
+	public Curso(Integer identificador, String descripcion) {
+		this.identificador = identificador;
+		this.descripcion = descripcion;		
+	}
+
+	public Integer getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(Integer identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public String toString() {
+		return "Curso [identificador=" + identificador + ", descripcion=" + descripcion + "]";
+	}
+	
+}
