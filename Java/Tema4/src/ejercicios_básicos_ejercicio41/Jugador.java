ea82f5186144e184512046512ab9d664bdc45a8e
JuanGabrielsv <juangabrielsv@gmail.com>
2024-01-29 19:03:51 +0100

    .

--- Java/Tema4/src/ejercicios_básicos_ejercicio41/Jugador.java
+++ Java/Tema4/src/ejercicios_básicos_ejercicio41/Jugador.java
@@ -62,6 +62,8 @@ partido.
 */
 package ejercicios_básicos_ejercicio41;
 
+import java.util.Objects;
+
 public class Jugador {
 	private String nombre;
 	private Integer dorsal;
@@ -71,11 +73,28 @@ public class Jugador {
 		this.dorsal = dorsal;
 	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dorsal);
+	}
+
 	@Override
 	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return super.equals(obj);
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jugador other = (Jugador) obj;
+		return Objects.equals(dorsal, other.dorsal);
 	}
+	
+	
 	
 
 }
