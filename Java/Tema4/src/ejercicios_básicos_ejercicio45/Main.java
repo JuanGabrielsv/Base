65b956d42460a733030cacc3e50229e5fb1e0cc6
JuanGabrielsv <juangabrielsv@gmail.com>
2024-02-05 16:19:32 +0100

    .

---
+++ Java/Tema4/src/ejercicios_básicos_ejercicio45/Main.java
@@ -0,0 +1,21 @@
+/* EJERCICIO 45
+Crea un programa que solicite al usuario su fecha de nacimiento. A continuación, usando la
+clase LocalDate, muéstrale la siguiente información:
+
+1. Si la fecha no es correcta, vuelve a solicitarla hasta que lo sea.
+2. Dile si nació o no en un año bisiesto.
+3. Dile el día de la semana en el que nació.
+4. Dile cuántos años tiene.
+5. Dile, suponiendo que va a vivir exactamente 100 años, cuánto tiempo de vida le queda (días, meses y años)
+6. Dile, ya de regalo, que hora es ahora en formato hora/minuto/segundo con 24 horas. Por ejemplo: 17:23:55  */
+
+package ejercicios_básicos_ejercicio45;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
