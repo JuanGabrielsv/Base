330aa04c5fefc7e3e1b3de7bd917ecf9369837ce
JuanGabrielsv <juangabrielsv@gmail.com>
2024-02-05 17:55:31 +0100

    .

--- Java/Tema4/src/ejercicios_básicos_ejercicio45/Main.java
+++ Java/Tema4/src/ejercicios_básicos_ejercicio45/Main.java
@@ -11,11 +11,64 @@ clase LocalDate, muéstrale la siguiente información:
 
 package ejercicios_básicos_ejercicio45;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
 
+		Scanner sc = new Scanner(System.in);
+		Boolean check = true;
+		LocalDate hoy = LocalDate.now();
+		LocalDate nacimiento;
+
+		do {
+			
+			System.out.println("Introduce tu fecha de nacimiento EJ: dd/MM/yyyy: ");
+			String fecha = sc.nextLine();
+			
+			Integer dia = Integer.parseInt(fecha.split("/")[0]);
+			Integer mes = Integer.parseInt(fecha.split("/")[1]);
+			Integer anyo = Integer.parseInt(fecha.split("/")[2]);
+			
+			nacimiento = LocalDate.of(anyo, mes, dia);
+			
+			if (dia >= 1 && dia <= 31 && mes >= 1 && mes <= 12 && anyo <= hoy.getYear()) {				
+				check = false;
+			}
+			else {
+				System.out.println("Fecha correcta?");
+				
+				check = true;
+			}			
+
+		} 
+		while (check);
+		
+		System.out.println("¿Naciste en un año bisiesto? " + nacimiento.isLeapYear());
+		
+		System.out.println("Naciste un " + nacimiento.getDayOfWeek());
+		
+		Period periodo = nacimiento.until(hoy);
+		System.out.println("Tienes " + periodo.getYears() + " años");
+		
+		LocalDate muerte = nacimiento.plusYears(100);
+		periodo = hoy.until(muerte);
+		System.out.println("Te quedan: " + periodo.getYears() + " años " + periodo.getMonths() + " meses " + periodo.getDays() + " días ");
+		
+		LocalTime hora = LocalTime.now();
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
+		System.out.println("Hora actual: " + formato.format(hora));
+		
+		sc.close();
+		
+		
+		
+		sc.close();
 	}
 
 }
