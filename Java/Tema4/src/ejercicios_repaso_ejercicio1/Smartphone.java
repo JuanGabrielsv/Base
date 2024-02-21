d9ffdc54123c444e74bb7d3c03312974e519c23c
JuanGabrielsv <juangabrielsv@gmail.com>
2024-02-21 16:51:31 +0100

    .

--- Java/Tema4/src/ejercicios_repaso_ejercicio1/Smartphone.java
+++ Java/Tema4/src/ejercicios_repaso_ejercicio1/Smartphone.java
@@ -36,34 +36,38 @@ package ejercicios_repaso_ejercicio1;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 
 public class Smartphone {
-	
+
 	private String marca;
 	private String modelo;
 	private String imei;
 	private BigDecimal precio;
 	private LocalDate fechaFabricacion;
 	private Boolean activo;
-	
+
+// Constructor por defecto.	
+
 	public Smartphone() {
-		
+
 		this.activo = true;
 		this.precio = BigDecimal.ZERO;
-		
+
 	}
-	
+
 // Constructor que reciba e inicialice el imei
-	
+
 	public Smartphone(String inputImei) {
-		
+
 		this.imei = inputImei;
 		this.activo = true;
 		this.precio = BigDecimal.ZERO;
 	}
-	
+
 // Constructor que reciba e inicialice el imei, la marca y el modelo
-	
+
 	public Smartphone(String inputImei, String inputMarca, String inputModelo) {
 		this.imei = inputImei;
 		this.activo = true;
@@ -71,33 +75,32 @@ public class Smartphone {
 		this.marca = inputMarca;
 		this.modelo = inputModelo;
 	}
-	
+
 // setPrecio() -> permite cambiar el precio del Smartphone
-	
+
 	public void setPrecio(BigDecimal precio) {
 		this.precio = precio;
 	}
-	
+
 // activar -> activa el Smartphone
-	
+
 	public void activar() {
 		this.activo = true;
 	}
-	
+
 // desactivar -> inactiva el Smartphone
-	
+
 	public void desactivar() {
 		this.activo = false;
 	}
-	
+
 //  getEdad() -> devuelve los años transcurridos desde el 1 de abril del 2000 hasta la fecha de fabricación del Smartphone.
-	
-	public LocalDate getEdad() {
-		return null;
+
+	public Integer getEdad() {
+		LocalDate fecha2000 = LocalDate.of(2000, Month.APRIL, 1);
+		Period periodo = fecha2000.until(fechaFabricacion);
+		return periodo.getYears();
 
 	}
-	
-	
-	
 
 }
