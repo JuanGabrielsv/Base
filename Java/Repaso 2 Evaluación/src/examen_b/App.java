53d6e716cc717ef20271c905ac74af37c74329cb
JuanGabrielsv <juangabrielsv@gmail.com>
2024-03-12 22:18:02 +0100

    .

--- Java/Repaso 2 Evaluación/src/examen_b/App.java
+++ Java/Repaso 2 Evaluación/src/examen_b/App.java
@@ -1,4 +1,4 @@
-package examen_taller;
+package examen_b;
 
 import java.text.DecimalFormat;
 import java.time.LocalDate;
--- Java/Repaso 2 Evaluación/src/examen_b/Reparacion.java
+++ Java/Repaso 2 Evaluación/src/examen_b/Reparacion.java
@@ -1,4 +1,4 @@
-package examen_taller;
+package examen_b;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
