e0e5c8cc72331ca82921ef6b75dd50cb74f063f3
JuanGabrielsv <juangabrielsv@gmail.com>
2024-03-13 23:04:05 +0100

    .

--- Java/Repaso 2 Evaluación/src/ejercicio_27/Alumno.java
+++ Java/Repaso 2 Evaluación/src/ejercicio_27/Alumno.java
@@ -1,4 +1,13 @@
-package ejercicio_26;
+/* EJERCICIO 27
+Crea un método validarDNI() en la clase Alumno que devuelva un Boolean indicando si el dni 
+que tiene establecido el alumno es correcto o no. Para ello, tendrás que validar lo siguiente:
+
+a) El dni no puede ser null ni vacío
+b) El dni tiene que tener una longitud total de 9.
+
+Modifica el programa del ejercicio 25 para validar el DNI de los alumnos que vas creando. Si alguno no es correcto, vuelve a solicitarlo. 
+*/
+package ejercicio_27;
 
 import java.util.Objects;
 
