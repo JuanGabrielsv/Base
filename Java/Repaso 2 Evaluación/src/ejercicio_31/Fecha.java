662f7acbb732b9bd884d74f35576ad25bfb2cffd
JuanGabrielsv <juangabrielsv@gmail.com>
2024-03-14 11:02:58 +0100

    .

---
+++ Java/Repaso 2 Evaluación/src/ejercicio_31/Fecha.java
@@ -0,0 +1,16 @@
+/*
+PARTE 1
+• Vamos a crear una clase Fecha que nos permita trabajar con este tipo de dato. 
+• Piensa un diseño para la clase, es decir, piensa que atributos y métodos debería de tener.
+• Ponte en el lugar de una empresa que está desarrollando esta clase y su objetivo luego es utilizarla en
+  todos sus programas o incluso venderla para que la usen otras empresas.
+
+PARTE 2
+•Implementa el diseño que habéis decidido de mutuo acuerdo entre todos. Crea un programa
+ que utilice la clase Fecha.
+*/
+package ejercicio_31;
+
+public class Fecha {
+
+}
