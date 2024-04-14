0edef4f6c27099f524ad94ef4e1795d0716fd055
juangabrielsv <juangabrielsv@gmail.com>
2024-04-14 13:47:43 +0200

    .

---
+++ Java/Tema5/src/ejercicios_básicos_04/App.java
@@ -0,0 +1,63 @@
+/*
+EJERCICIO 4
+En el programa anterior, cuando te han indicado una posición que no existe en la lista, salta
+una excepción y el programa termina. Captura dicha excepción para que en tal caso el número
+devuelto sea null. 
+*/
+package ejercicios_básicos_04;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Integer entradaUsuario;
+		Boolean check = true;
+		SacoNumeros listaNumeros = new SacoNumeros();
+
+		try {
+			do {
+				try {
+					System.out.println("INTRODUCE UN NÚMERO ENTERO: ");
+					entradaUsuario = Integer.parseInt(sc.nextLine());
+					if (entradaUsuario.equals(-1)) {
+						check = false;
+					} else {
+						listaNumeros.addNumero(entradaUsuario);
+					}
+				} catch (NumberFormatException e) {
+					System.out.println("Debes introducir un número entero, no valen letras, ni espacios en blanco.");
+				}
+			} while (check);
+
+			System.out.println(listaNumeros);
+			check = true;
+
+			do {
+				try {
+					Integer entradaUsuario2;
+					System.out.println("INTRODUCE UN NÚMERO ENTERO QUE INDICA LA POSICIÓN EN LA LISTA: ");
+					entradaUsuario2 = Integer.parseInt(sc.nextLine());
+					if (entradaUsuario2 == -1) {
+						System.out.println("- SALIENDO DEL PROGRAMA -");
+						check = false;
+					} else {
+						System.out.println(listaNumeros.getNumero(entradaUsuario2));
+					}
+				} catch (NumberFormatException y) {
+					System.err.println("NO SE ADMITE INTRODUCIR UN ESPACIO EN BLANCO o NO INTRODUCIR NADA");
+				} catch (IndexOutOfBoundsException e) {
+					System.out.println("kjhdf");
+					check = false;				
+				}
+			} while (check);
+		} finally {
+			sc.close();
+			System.out.println("Escaner cerrado correctamente");
+		}
+
+	}
+
+}
---
+++ Java/Tema5/src/ejercicios_básicos_04/SacoNumeros.java
@@ -0,0 +1,60 @@
+/*
+EJERCICIO 4
+En el programa anterior, cuando te han indicado una posición que no existe en la lista, salta
+una excepción y el programa termina. Captura dicha excepción para que en tal caso el número
+devuelto sea null.  
+*/
+package ejercicios_básicos_04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SacoNumeros {
+	
+	List<Integer> listaNumeros;
+	
+	//GETTERS AND SETTERS.
+
+	public List<Integer> getListaNumeros() {
+		return listaNumeros;
+	}
+
+	public void setListaNumeros(List<Integer> listaNumeros) {
+		this.listaNumeros = listaNumeros;
+	}
+	
+	//CONSTRUCTORES.
+	
+	public SacoNumeros() {
+		this.listaNumeros = new ArrayList<Integer>();
+	}
+	
+	//MÉTODOS.
+	// La clase SacoNumeros tendrá que tener un método addNumero() que te permita ir añadiendo dichos números.
+	
+	public void addNumero(Integer numero) {
+		this.getListaNumeros().add(numero);
+	}
+	
+	// Otro método toString() que imprima todos los números.
+
+	@Override
+	public String toString() {
+		return "SacoNumeros [listaNumeros=" + listaNumeros + "]";
+	}
+	
+	// Una vez que lo tengas modificado, añade otro método que sea getNumero() que recibe un
+	// Integer que será la posición y devuelve el número que está en dicha posición.
+	
+	public Integer getNumero(Integer posicion) throws IndexOutOfBoundsException {
+		try {
+			return this.listaNumeros.get(posicion);			
+		} catch (IndexOutOfBoundsException e) {
+			return null;			
+		}	
+		
+	}
+	
+	
+
+}
