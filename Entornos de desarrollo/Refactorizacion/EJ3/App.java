5470cb5f9f925c9672421294e48a66c165ec0849
JuanGabrielsv <juangabrielsv@gmail.com>
2024-04-26 18:58:41 +0200

    .

---
+++ Entornos de desarrollo/Refactorizacion/EJ3/App.java
@@ -0,0 +1,23 @@
+package ejercicio3;
+
+public class App {
+
+	public static void main(String[] args) {
+		 // Crear un objeto Producto
+        Producto producto = new Producto("Camiseta", 20.99, 50);
+
+        // Mostrar información del producto
+        System.out.println("Nombre del producto: " + producto.Nombre);
+        System.out.println("Precio del producto: $" + producto.precio);
+        System.out.println("Cantidad disponible: " + producto.cantidadDisponible);
+
+        // Realizar algunas operaciones con el producto
+        producto.aumentarCantidad(10);
+        producto.reducirCantidad(5);
+
+        // Calcular el total para una cantidad específica
+        int cantidad = 3;
+        double total = producto.calcularTotal(cantidad);
+        System.out.println("Total para " + cantidad + " productos: $" + total);
+    }
+}
