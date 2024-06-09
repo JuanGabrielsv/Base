32ec4810fc1444dde3b63e7dcd2d91189c30fb33
JuanGabrielsv <juangabrielsv@gmail.com>
2024-06-09 23:10:04 +0200

    Parchis2

--- Aprendiendo Javascript/Tema 2 - Estructuras de control/Ejercicios - Tema 2/Ejercicio_65/main.js
+++ Aprendiendo Javascript/Tema 2 - Estructuras de control/Ejercicios - Tema 2/Ejercicio_65/main.js
@@ -26,22 +26,107 @@ Cuando ocurra algo así, debes avisar al usuario de lo ocurrido.
 
 import { tirarDado } from "./funciones.js";
 
+const CASILLA19 = 19;
+const CASILLA31 = 31;
+const CASILLA56 = 56;
+
 let posicionJugador = 0;
 let posicionMaquina = 0;
 let numeroSacado = 0;
+let turnoGlobal = 0;
+let penalizacion = 0;
+let turnoJugador = true;
+let turnoMaquina = true;
 let check = true;
 
 
 
 do {
-  alert("\nJugador: posicion " + posicionJugador + "\nMáquina: posicion " + posicionMaquina + "\n\nTurno del JUGADOR. Pulse ENTER para tirar el dado.");
-  numeroSacado = tirarDado();
-  if (numeroSacado === 6) {
-    posicionJugador += numeroSacado;
-    alert("HAS SACADO UN " + numeroSacado + "!!\n\nAvanzas a la posición: " + posicionJugador);
+  turnoGlobal++;
+
+  while (turnoJugador) {
     
+    alert("\nJugador: posicion " + posicionJugador + "\nMáquina: posicion " + posicionMaquina + "\n\nTurno del JUGADOR. Pulse ENTER para tirar el dado.");
+    numeroSacado = tirarDado();
+    posicionJugador += numeroSacado;
+
+    if (posicionJugador > 60) {
+      
+    }
+
+    while (posicionJugador < 60) {
+      
+    }
+
+    if (numeroSacado === 6) {
+      posicionJugador += numeroSacado;
+      alert("HAS SACADO UN " + numeroSacado + " repites turno...");
+      turnoJugador = true;
+    }
+
+    alert("Has sacado un " + numeroSacado + "\nAvanzas a la posicion " + posicionJugador);
+
+    do {
+      if (posicionJugador % 5 === 0) {
+        posicionJugador += 5;
+        alert("Has caido en una Oca\nTu nueva posición es: " + posicionJugador + " vuelve a tirar el dado, presiona ENTER");
+        numeroSacado = tirarDado();
+        posicionJugador += numeroSacado;
+        alert("Sacas un " + numeroSacado + " y continuas a la casilla " + posicionJugador);
+      }
+    } while (numeroSacado === 5);
+
+    if (posicionJugador === CASILLA19) {
+      alert("Oh que pena, caes en la casilla 19. Pierdes 1 turno");
+      turnoJugador = false;
+      penalizacion = 1;
+    } else if (posicionJugador === CASILLA31) {
+      turnoJugador = false;
+      alert("Oh que pena, caes en la casilla 31. Pierdes 1 turno");
+      penalizacion = 2;
+    } else if (posicionJugador === CASILLA56) {
+      turnoJugador = false;
+      alert("Oh que pena, caes en la casilla 56. Pierdes 1 turno");
+      penalizacion = 3;
+    }
+
+      
+
+    
+
+    
+  
   }
-  
-  
   check = false;
 } while (check);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*if (posicionJugador % 5 === 0) {
+        posicionJugador += 5;
+        alert("Has caido en un Oca, y tiro por que me toca !\nHas pasado a la posición: " + posicionJugador + "\nPresiona ENTER para volver a tirar el dado.");
+        numeroSacado = tirarDado();                       
+      } 
+
+      if (posicionJugador === CASILLA19) {        
+        alert("Has caido en la casilla " + posicionJugador + ", pierdes un turno.");        
+        penalizacion = 1;        
+      } else if (posicionJugador === CASILLA31) {
+        alert("Has caido en la casilla " + posicionJugador + ", pierdes dos turnos.");        
+        penalizacion = 2;
+      } else if (posicionJugador === CASILLA56) {
+        alert("Has caido en la casilla " + posicionJugador + ", pierdes tres turnos.");        
+        penalizacion = 3;
+      }*/
