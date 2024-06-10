0b44e56b1280f085d73247c6b21db98332825e86
JuanGabrielsv <juangabrielsv@gmail.com>
2024-06-10 19:41:05 +0200

    .

---
+++ Aprendiendo Javascript/Tema 2 - Estructuras de control/Ejercicios - Tema 2/Ejercicio_66/main.js
@@ -0,0 +1,12 @@
+/*
+EJERCICIO 66
+El juego consiste en adivinar un número aleatorio generado por el ordenador entre el rango que le indiques. 
+Se le preguntará al usuario:
+
+  1 - El número inicial del rango.
+  2 - El número final del rango.
+  3 - El número de intentos en los que crees que vas a adivinar el número secreto.
+
+Se generará el número aleatorio entre los rangos marcados por el usuario.
+EL usuario tendrá que ir proponiendo valores mientras que le queden intentos por adivinar. 
+*/
