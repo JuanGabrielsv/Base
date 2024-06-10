d5f7bcc7b237a64d9a78ed45a94aca689368e1fc
JuanGabrielsv <juangabrielsv@gmail.com>
2024-06-10 22:12:43 +0200

    Terminado ejercicio 66 Aprendiendo JavaScript

--- Aprendiendo Javascript/Tema 2 - Estructuras de control/Ejercicios - Tema 2/Ejercicio_66/funciones.js
+++ Aprendiendo Javascript/Tema 2 - Estructuras de control/Ejercicios - Tema 2/Ejercicio_66/funciones.js
@@ -1,3 +1,14 @@
 export function tirarDado() {
     return Math.floor(Math.random() * 6) + 1;
 }
+
+
+/* GENERA NÚMERO RANDOM ENTRE DOS NÚMEROS QUE LE PASEMOS */
+export function generarNumeroRandomEntreDosNumeros(min, max) {
+    if (min > max) {
+        let nuevoMin = min;
+        min = max;
+        max = nuevoMin;
+    }
+    return Math.floor(Math.random() * (Number(max) - Number(min) + 1)) + Number(min);
+}
