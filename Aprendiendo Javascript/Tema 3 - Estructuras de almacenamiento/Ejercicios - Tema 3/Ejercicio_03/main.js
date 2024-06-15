e13fc4ca825dc9f5f1d7382b6faf94a08afc0f60
JuanGabrielsv <juangabrielsv@gmail.com>
2024-06-15 23:43:04 +0200

    Terminado ejercicio

--- Aprendiendo Javascript/Tema 3 - Estructuras de almacenamiento/Ejercicios - Tema 3/Ejercicio_03/main.js
+++ Aprendiendo Javascript/Tema 3 - Estructuras de almacenamiento/Ejercicios - Tema 3/Ejercicio_03/main.js
@@ -15,10 +15,16 @@ Todo esto se debe hacer recorriendo el array.
 
 const arrayNumeros = new Array();
 
-let arrayNumerosInversos = []; //Otra forma de declarar una array.
+let arrayNumerosInversos = [];
 let tamanoDelArray;
 let vNumero;
 let vNumeroIntroducido;
+let vNumeroMayor;
+let vNumeroMenor;
+let vPosicionMayor;
+let vPosicionMenor;
+let vSuma = 0;
+let vMedia;
 let check = true;
 
 /* En primer lugar, preguntaremos al usuario cuántos números nos va a decir. */
@@ -31,6 +37,7 @@ do {
     }
 } while (!Number.isInteger(vNumero));
 
+// 2. Luego se los iremos solicitando uno a uno y los iremos guardando en un array. 
 /* Luego se los iremos solicitando uno a uno y los iremos guardando en un array. */
 do {
     for (let i = 0; i < vNumero; i++) {
@@ -46,24 +53,68 @@ do {
     check = false
 } while (check)
 
+// 3. Al terminar, le mostraremos la lista completa de números.
 /* Imprimimos la array con el método .toString */
+console.log("Imprimimos la array con el método .toString()");
 console.log(arrayNumeros.toString());
 
 /* Tambien podemos usar un for */
+console.log("También podemos usar un FOR");
 for (let i = 0; i < arrayNumeros.length; i++) {
     console.log(arrayNumeros[i]);
 }
 
-/* Tambien con un foreach? */
+/* Tambien con un foreach */
+console.log("Con un FOREACH");
 arrayNumeros.forEach(element => {
     console.log(element);
 });
 
-/* Podemos usar el método .reverse() para guardar la array invertida en otra nueva y la imprimimos */
-arrayNumerosInversos = arrayNumeros.reverse();
-arrayNumerosInversos.forEach(element => {
-    console.log(element);
-});
+// 4. Muestra la lista completa de números en orden inverso a como fueron introducidos por el usuario.
+/* Lo hacemos con varios métodos y usando .slice */
+console.log("Lo hacemos con varios métodos y usando .slice");
+arrayNumeros.slice().reverse().forEach(element => console.log(element));
+
+/* Lo hacemos tambien con un for */
+console.log("Lo hacemos tambien con un for");
+for (let i = arrayNumeros.length - 1; i >= 0; i--) {
+    console.log(arrayNumeros[i]);
+}
+
+/* Lo mas lógico sería crear una array nueva copiando la otra y le damos la vuelta con .reverse() */
+console.log("Creamos un array nuevo que sea una copia del original y le damos la vuelta con .reverse()");
+arrayNumerosInversos = Array.from(arrayNumeros);
+arrayNumerosInversos.reverse();
+console.log(arrayNumerosInversos.toString());
+
+// 5. Muestra cual número es el mayor y el menor (y en qué posición están).
+/* Sacamos el número mayor */
+console.log("Sacamos el número mayor y menor");
+vNumeroMayor = arrayNumeros[0];
+vNumeroMenor = arrayNumeros[0];
+for (let i = 0; i < arrayNumeros.length; i++) {
+    if (arrayNumeros[i] > vNumeroMayor) {
+        vNumeroMayor = arrayNumeros[i];
+        vPosicionMayor = i;
+    }
+
+    if (arrayNumeros[i] < vNumeroMenor) {
+        vNumeroMenor = arrayNumeros[i];
+        vPosicionMenor = i;
+    }
+}
+console.log(vNumeroMayor + " en la posición " + vPosicionMayor);
+console.log(vNumeroMenor + " en la posición " + vPosicionMenor);
+console.log(arrayNumeros);
+
+// 6. Calcula la media de todos los números.
+console.log("Calculamos la media de todos los números");
+for (let i = 0; i < arrayNumeros.length; i++) {
+    vSuma += arrayNumeros[i];
+}
+vMedia = Number(vSuma / arrayNumeros.length);
+console.log(vMedia);
+
 
 
 
