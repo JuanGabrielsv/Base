848179f8268a0521c9f262ab4c20ebc43d4a91a8
JuanGabrielsv <juangabrielsv@gmail.com>
2024-06-15 21:15:19 +0200

    Continuamos

--- Aprendiendo Javascript/Tema 3 - Estructuras de almacenamiento/Ejercicios - Tema 3/Ejercicio_03/main.js
+++ Aprendiendo Javascript/Tema 3 - Estructuras de almacenamiento/Ejercicios - Tema 3/Ejercicio_03/main.js
@@ -15,30 +15,55 @@ Todo esto se debe hacer recorriendo el array.
 
 const arrayNumeros = new Array();
 
+let arrayNumerosInversos = []; //Otra forma de declarar una array.
 let tamanoDelArray;
 let vNumero;
+let vNumeroIntroducido;
 let check = true;
 
-/* En primer lugar, preguntaremos al usuario cuántos números nos va a decir. */ 
+/* En primer lugar, preguntaremos al usuario cuántos números nos va a decir. */
 do {
     vNumero = prompt("¿Cuántos números vas a introducir?")
     if (vNumero !== null && vNumero !== "" && !isNaN(vNumero)) {
         vNumero = parseInt(vNumero)
     } else {
         alert("Datos introducidos no correctos, introduce un número entero...")
-    }    
+    }
 } while (!Number.isInteger(vNumero));
 
 /* Luego se los iremos solicitando uno a uno y los iremos guardando en un array. */
-
-
-/*let N;
+do {
+    for (let i = 0; i < vNumero; i++) {
         do {
-            let input = prompt("Introduce un valor entero mayor a 0 para N:");
-            if (input !== null && input !== '' && !isNaN(input)) {
-                N = parseInt(input);
+            vNumeroIntroducido = prompt("Números a introducir: " + (vNumero - i) + "\n\nIntroduce el número:");
+            if (vNumeroIntroducido !== null && vNumeroIntroducido !== "" && !isNaN(vNumeroIntroducido)) {
+                arrayNumeros.push(Number(vNumeroIntroducido));
+            } else {
+                alert("Datos introducidos no correctos, introduce un número entero...")
             }
-        } while (!Number.isInteger(N) || N <= 0);
+        } while (vNumeroIntroducido === null || vNumeroIntroducido === "" || isNaN(vNumeroIntroducido));
+    }
+    check = false
+} while (check)
 
-        console.log(N);*/
+/* Imprimimos la array con el método .toString */
+console.log(arrayNumeros.toString());
+
+/* Tambien podemos usar un for */
+for (let i = 0; i < arrayNumeros.length; i++) {
+    console.log(arrayNumeros[i]);
+}
+
+/* Tambien con un foreach? */
+arrayNumeros.forEach(element => {
+    console.log(element);
+});
+
+/* Podemos usar el método .reverse() para guardar la array invertida en otra nueva y la imprimimos */
+arrayNumerosInversos = arrayNumeros.reverse();
+arrayNumerosInversos.forEach(element => {
+    console.log(element);
+});
+
+
 
