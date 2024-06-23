573366bc2cec13421b3600da11d8d0f58af25dbb
JuanGabrielsv <juangabrielsv@gmail.com>
2024-06-23 04:19:07 +0200

    .

--- Aprendiendo Javascript/Usando el DOM/06/js/main.js
+++ Aprendiendo Javascript/Usando el DOM/06/js/main.js
@@ -1,1 +1,9 @@
 const arrayNotas = [7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10];
+
+function cambiarModo () {
+
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    
+}
