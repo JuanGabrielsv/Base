c8616f4bb1976b9c7165d33420603a91ea8902af
Juangabrielsv <juangabrielsv@gmail.com>
2024-10-01 10:02:15 +0200

    .

---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Principios Programación JS/Ejercicio05/js/main.js
@@ -0,0 +1,9 @@
+/*5. La función isNaN, devuelve verdadero si la expresión no es numérica:
+1. ¿Qué devuelve console.log(isNaN(“Hola” * 5));?
+2. ¿Y console.log(isNaN(“3”*5)); ?*/
+
+// 1. ¿Qué devuelve console.log(isNaN(“Hola” * 5));?
+console.log(isNaN("Hola" * 5));
+
+//2. ¿Y console.log(isNaN(“3”*5)); ?*/
+console.log(isNaN("3" * 5));
