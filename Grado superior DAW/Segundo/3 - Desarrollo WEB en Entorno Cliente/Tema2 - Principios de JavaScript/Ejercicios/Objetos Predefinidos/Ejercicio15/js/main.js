b00683c73e341a4a5d5fe5a4f452ca02d538bdff
juangabrielsv <juangabrielsv@gmail.com>
2024-10-02 16:28:52 +0200

    añadido ejercicio15 desarrollo entorno cliente tema3 Objetos predefinidos

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Objetos Predefinidos/Ejercicio15/js/main.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Objetos Predefinidos/Ejercicio15/js/main.js
@@ -2,8 +2,14 @@
 la nueva ventana. La nueva ventana no tendrá barra de herramientas, ni location, ni barra de menú. Tendrá
 200x80 pixeles y y se posicionará en la posición 500x500. Será resizable y tendrá scrollbars.
 La nueva ventana incluirá el texto: “Entorno cliente” */
+const caracteristicas = "toolbar=yes, width=200, height=200";
 
 let eleccionUsuario = confirm("¿Quieres abrir una nueva ventana?");
-let nuevaVentana =
+let nuevaVentana;
+
+if (eleccionUsuario === true) {
+    nuevaVentana = window.open("", "Prueba", caracteristicas);
+}
+console.log(eleccionUsuario);
 
 
