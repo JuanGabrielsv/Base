3fbf8ed6c2f210498bfbceb5981c35a922282702
Juangabrielsv <juangabrielsv@gmail.com>
2024-10-02 13:50:14 +0200

    Añadido parte de la solución en el ejercicio02 del tema3 de desarrolo en entorno cliente JAVA

--- Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema3/Ejercicios/Rest Service/Ejercicio02/src/main/java/com/example/demo/modelo/Cliente.java
+++ Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema3/Ejercicios/Rest Service/Ejercicio02/src/main/java/com/example/demo/modelo/Cliente.java
@@ -1,5 +1,17 @@
 package com.example.demo.modelo;
 
 public class Cliente {
+	
+	Integer id;
+	String nombre;
+	String username;
+	String password;
+	
+	public Cliente(Integer id, String nombre, String username, String password) {
+		this.id = id;
+		this.nombre = nombre;
+		this.username = username;
+		this.password = password;
+	}
 
 }
