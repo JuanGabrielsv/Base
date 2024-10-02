bbbf512e87bcc1416b3ae37bf3db5f58e988f428
juangabrielsv <juangabrielsv@gmail.com>
2024-10-02 16:07:37 +0200

    .

---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Objetos Predefinidos/Ejercicio15/js/main.js
@@ -0,0 +1,9 @@
+/* 15. Crear un programa que pregunte al usuario si acepta o no abrir una ventana nueva. Sólo si acepta, se abrirá
+la nueva ventana. La nueva ventana no tendrá barra de herramientas, ni location, ni barra de menú. Tendrá
+200x80 pixeles y y se posicionará en la posición 500x500. Será resizable y tendrá scrollbars.
+La nueva ventana incluirá el texto: “Entorno cliente” */
+
+let eleccionUsuario = confirm("¿Quieres abrir una nueva ventana?");
+let nuevaVentana =
+
+
