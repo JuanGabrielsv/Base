f011af11bea3253d9793b694feb359e161b2ebcd
Juangabrielsv <juangabrielsv@gmail.com>
2024-10-03 10:44:20 +0200

    .

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Objetos Predefinidos/Ejercicio04/js/main.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Objetos Predefinidos/Ejercicio04/js/main.js
@@ -1,9 +1,14 @@
-/* 4. Crear un programa que muestre la fecha actual en distintos formatos:
-1. dd/mm/yyyy
-2. Dia de la semana mes día año, en inglés: Ej Mon Sep 30 2024 
-3. dd/mm/yyyy hh:mm:ss
-4. Hh:mm:ss */
-
+// 4. Crear un programa que muestre la fecha actual en distintos formatos:
 let fechaUno = new Date();
 
-console.log(fechaUno);
+//1. dd/mm/yyyy.
+console.log(fechaUno.toLocaleDateString());
+
+//2. Dia de la semana mes día año, en inglés: Ej Mon Sep 30 2024.
+document.write(fechaUno.toDateString());
+
+//3. dd/mm/yyyy hh:mm:ss.
+document.write(`<br>` + fechaUno.toLocaleString());
+
+//4. Hh:mm:ss
+document.write(`<br>` + fechaUno.toLocaleTimeString());
