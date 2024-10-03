008a1c97e00a09b07daa057a6d0b424036246c07
Juangabrielsv <juangabrielsv@gmail.com>
2024-10-03 10:34:21 +0200

    f

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Objetos Predefinidos/Ejercicio03/js/main.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Objetos Predefinidos/Ejercicio03/js/main.js
@@ -3,12 +3,17 @@ años en que tu cumpleaños va a caer en domingo desde este año hasta el año 2
 NOTA: los meses comienzan por 0. */
 
 let diaCumpleanos = Number(prompt("Introduce el día de tu cumpleaños"));
-let mesCumpleanos = Number(prompt("Introduce el mes de tu cumpleaños")) + 1;
+let mesCumpleanos = Number(prompt("Introduce el mes de tu cumpleaños")) - 1;
 let anoActual = new Date().getFullYear();
 
 for (let ano = anoActual; ano <= 2100; ano++) {
-    let fechaCumpleano = new Date(ano, mesCumpleanos, diaCumpleanos);
-
+    var fechaCumpleano = new Date(ano, mesCumpleanos, diaCumpleanos);
+    if (fechaCumpleano.getDay() === 0) {
+        document.write(fechaCumpleano.getFullYear() + `<br>`);
+    }
 }
+
+console.log(fechaCumpleano.getDay());
+console.log(fechaCumpleano.getDate());
 
 
