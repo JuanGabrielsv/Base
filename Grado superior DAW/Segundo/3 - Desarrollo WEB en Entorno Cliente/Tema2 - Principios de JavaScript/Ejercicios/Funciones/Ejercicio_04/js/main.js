1daa8f3a6df504ace00ec694c7e967a6e8d2e699
Juangabrielsv <juangabrielsv@gmail.com>
2024-10-11 22:22:59 +0200

    ejercicios resueltos

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Funciones/Ejercicio_04/js/main.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Funciones/Ejercicio_04/js/main.js
@@ -1,3 +1,28 @@
 /*4. Crear una función flecha que escriba por pantalla el sumatorio de un número. El programa debe solicitar al
 usuario un número mayor que 0. Comprobar que el número es válido.
 Ejemplo: sumatorio de 5 es: 5+4+3+2+1 = 15*/
+
+let entradaUsuario;
+let sumatorio = 0;
+let mensaje = "";
+let calcularSumatorio = (entradaUsuario) => {
+    for (let i = entradaUsuario; i > 0; i--) {
+        sumatorio += i;
+        if (i !== 1) {
+            mensaje += `${i} + `;
+        } else {
+            mensaje += `${i} = `;
+        }
+    }
+    mensaje += sumatorio;
+    document.body.innerHTML += `<p>El sumatorio de ${entradaUsuario} es: ${mensaje}</p>`;
+}
+
+do {
+    entradaUsuario = Number(prompt("Introduce un número mayor que 0: "));
+} while (entradaUsuario <= 0 || isNaN(entradaUsuario) == true);
+
+calcularSumatorio(entradaUsuario);
+
+
+
---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Funciones/Ejercicio_05/js/main.js
@@ -0,0 +1,37 @@
+/*
+5. Crear una función flecha que permita dibujar una tabla en una página web. 
+
+• Como parámetros tendrá el numero de filas y de columnas: Por defecto 10 y 4. 
+• También tendrá un tercer parámetro que permite indicar el color del borde, que por defecto será negro.
+• La tabla se crea con un borde de 1 pixel entre cada celda. 
+• El borde exterior tendrá 3 pixeles y será del mismo color que el borde de las celdas.
+• La tabla ocupará toda la anchura de la página.
+
+1) Llamar a la función para crear la tabla con borde negro, de 10 filas y 4 columnas.
+2) Llamarla de nuevo para generar la table de 20 filas, 10 col con borde negro.
+3) Dibujar 10 tablas de 5 filas y 4 columnas con borde verde.
+*/
+
+let numColumnas;
+let numFilas;
+let colorBorde;
+let bordeEntreCadaCelda = "1px";
+let bordeExterior = "3px";
+let dibujarTabla = (numFilas = 10, numColumnas = 4, colorBorde = "black") => {
+
+    document.body.innerHTML = `<table id="tablita" style = "border: ${colorBorde} 3px solid; width:100%;"></table>`;
+    document.getElementById("tablita").innerHTML = `<tbody id="bodisito"></tbody>`;
+
+    for (let i = 0; i < numFilas; i++) {
+        document.getElementById("bodisito").innerHTML += `<tr id="filita${i}"></tr>`;
+        for (let j = 0; j < numColumnas; j++) {
+            document.getElementById(`filita${i}`).innerHTML += `<td style = "border: black 1px solid; height:10px;"></td>`;
+        }
+    }
+}
+
+dibujarTabla(18, 4, "black");
+dibujarTabla(14, 4, "black");
+
+
+
