62be7074e977a8b1be5d1f0d91dd4ac884f7708c
Juangabrielsv <juangabrielsv@gmail.com>
2024-10-20 13:45:32 +0200

    .

---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Arrays y Objetos/Ejercicio04/js/main.js
@@ -0,0 +1,55 @@
+/* 4. Crear un array de objetos que representan productos en una tienda.
+
+• Donde cada objeto tiene las propiedades nombre, precio y categoria:
+• Obtener un nuevo array que contenga sólo los nombres de los productos, transformando cada nombre en mayúsculas.
+• Mostrar en consola el array de nombres transformados y en la página HTML separados por guión. */
+
+
+let bebida = {
+    nombre: "cocacola",
+    precio: 2.25,
+    categoria: "Bebidas Gaseosas"
+}
+
+let comida = {
+    nombre: "Potage",
+    precio: 1.50,
+    categoria: "Guisos"
+}
+
+let snack = {
+    nombre: "bocabit",
+    precio: 1,
+    categoria: "patatas"
+}
+
+let aObjetos = [bebida, comida, , , , snack];
+let aNombreObjetos = [];
+
+/*for (let i in aObjetos) {
+    aNombreObjetos.push(aObjetos[i].nombre.toUpperCase());    
+}
+document.body.innerHTML = aNombreObjetos;*/
+
+/*let aNombreObjetos = aObjetos.map(elemento => elemento.nombre.toUpperCase());
+document.body.innerHTML = aNombreObjetos;*/
+
+aObjetos.forEach(element => {
+    aNombreObjetos.push(element.nombre.toUpperCase());    
+});
+document.body.innerHTML = aNombreObjetos;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/Arrays y Objetos/Ejercicio09/js/main.js
@@ -0,0 +1,23 @@
+/* 9. Crear un objeto literal llamado factura con las propiedades:
+
+    1. numero, cliente, divisa, subtotal e IVA, dándole valores a cada uno de ellos.
+    2. Tendrá también un método que calcula el total ( subtotal + iva).
+    3. Imprimir por consola: La factura X(numero) tiene un importe de Y(subtotal) Z(divisa).
+*/
+
+let factura = {
+    numero: 1,
+    cliente: "pepe",
+    divisa: "€",
+    subtotal: 12.55,
+    iva: 21,
+
+    total: function(){        
+        let ivaAPagar = (this.iva / 100) * this.subtotal;
+        return this.subtotal + ivaAPagar;
+    }
+
+}
+
+document.body.innerHTML = `La Factura número ${factura.numero} tiene un importe ${factura.total()}${factura.divisa}`;
+
