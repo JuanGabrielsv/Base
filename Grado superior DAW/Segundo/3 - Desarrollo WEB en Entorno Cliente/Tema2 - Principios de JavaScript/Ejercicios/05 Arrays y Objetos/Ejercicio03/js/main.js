b42fff7dd275816ff7efe9131407c6663ce05a98
juangabrielsv <juangabrielsv@gmail.com>
2024-10-21 19:18:58 +0200

    Termiando ejercicio03 de objetos en desarrollo entorno cliente y renombrado de muchas carpetas

---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/05 Arrays y Objetos/Ejercicio03/js/main.js
@@ -0,0 +1,65 @@
+/* 3. Crear un array de 5 elementos que representan personas, donde cada elemento tiene las propiedades: 
+
+    nombre, edad y ciudad.
+
+    1. Mostrar en la página las personas mayores de edad.
+    2. Mostrar en la página  las personas que son de Sevilla */
+
+let persona1 = {
+    nombre: "Laura",
+    edad: 27,
+    ciudad: "Sevilla"
+}
+
+let persona2 = {
+    nombre: "Pepe",
+    edad: 17,
+    ciudad: "Málaga"
+}
+
+let persona3 = {
+    nombre: "Carmen",
+    edad: 37,
+    ciudad: "Sevilla"
+}
+
+let persona4 = {
+    nombre: "Lidia",
+    edad: 17,
+    ciudad: "Sevilla"
+}
+
+let persona5 = {
+    nombre: "Mamen",
+    edad: 57,
+    ciudad: "Badajoz"
+}
+
+let aPersonas = [persona1, persona2, persona3, persona4, persona5];
+
+let mayorEdad = aPersonas.filter((persona) => persona.edad >= 18);
+mayorEdad.forEach(persona => {
+    document.body.innerHTML += `${persona.nombre}<br>`;
+});
+
+document.body.innerHTML += `<br>`;
+
+let ciudadSevilla = aPersonas.filter((persona) => persona.ciudad == "Sevilla");
+ciudadSevilla.forEach(persona => {
+    document.body.innerHTML += `${persona.nombre}<br>`;
+});
+
+
+
+
+/*let contador = 0;
+aPersonas.forEach(persona => {
+    if (persona.edad >= 18) {
+        document.body.innerHTML += `${persona.nombre}<br>`;
+        contador++;
+    }
+    if(contador == 0){
+        document.body.innerHTML = "No hay personas mayor de edad";
+    }    
+});*/
+
