062e1360c298694368ba112e97501f910649611e
juangabrielsv <juangabrielsv@gmail.com>
2024-10-23 20:43:21 +0200

    .

--- Grado superior DAW/Segundo/2 - Desarrollo WEB en Entornos Servidor - PHP/Práctica primer trimestre/src/php/resultado.php
+++ Grado superior DAW/Segundo/2 - Desarrollo WEB en Entornos Servidor - PHP/Práctica primer trimestre/src/php/resultado.php
@@ -1,5 +1,52 @@
 <?php
 
-print_r($_REQUEST);
+if (!isset($_REQUEST["tipoenvase"])) {
+    print "<p>No se ha marcado el tipo de envase</p>";
+    $check = false;    
+} else {
+    $tipoEnvase = $_REQUEST["tipoenvase"];
+    $check = true;
+}
+
+if (($_REQUEST["marcaproducto"]) == "") {
+    print "<p>Falta la marca del producto</p>";
+    $check = false;    
+} else {
+    $marcaProducto = $_REQUEST["marcaproducto"];
+    $check = true;
+}
+
+if (($_REQUEST["advertencia"]) == "") {
+    print "<p>No se ha indicado si el producto lleva o no advertencia por el consumo</p>";
+    $check = false;    
+} else {
+    $marcaProducto = $_REQUEST["advertencia"];
+    $check = true;
+}
+
+
+print_r( $_REQUEST);
+print "<br>";
+
+while ($check == true) {
+    print "<br>se ha entrado";
+    break;
+}
+
+/*
+
+$tipoCerveza = $_REQUEST["tipocerveza"];
+$denominacionAlimento = $_REQUEST["denominacionalimento"];
+//$tipoEnvase = $_REQUEST["tipoenvase"];
+$cantidadNeta = $_REQUEST["cantidadneta"];
+$marcaProducto = $_REQUEST["marcaproducto"];
+$advertencia = $_REQUEST["advertencia"];
+$fechaConsumo = $_REQUEST["fechaconsumo"];
+$alergenos = $_REQUEST["alergenos"];
+$observaciones = $_REQUEST["observaciones"];
+
+*/
+
+print "<br>Tipo de cerveza".$tipoCerveza;
 
 ?>
