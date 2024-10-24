f566c6e971e4e0ad57c1c4b9fae4243a15f94fe2
Juangabrielsv <juangabrielsv@gmail.com>
2024-10-24 11:48:29 +0200

    Terminado hasta el ejercicio16 Tema2 en desarrollo cliente

--- Grado superior DAW/Segundo/2 - Desarrollo WEB en Entornos Servidor - PHP/Práctica primer trimestre/src/php/resultado.php
+++ Grado superior DAW/Segundo/2 - Desarrollo WEB en Entornos Servidor - PHP/Práctica primer trimestre/src/php/resultado.php
@@ -1,52 +1,91 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aquí no se vende cruzcampo</title>
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<header>
+    <h1>Informe de inserción de cerveza</h1>
+</header>
+
+<body>
+
+
+
+</body>
+
+</html>
 <?php
+while (
+    !isset($_REQUEST["tipoenvase"]) ||
+    $_REQUEST["marcaproducto"] == "" ||
+    $_REQUEST["advertencia"] == "" ||
+    $_REQUEST["fechaconsumo"] == "" ||
+    !isset($_REQUEST["alergenos"])
+) {
+    print "<h2><span>ERROR</span> - hay algunos campos que no se han rellenado</h2>";
+    if (!isset($_REQUEST["tipoenvase"])) {
+        print "<p>No se ha marcado el tipo de envase</p>";
+    }
 
-if (!isset($_REQUEST["tipoenvase"])) {
-    print "<p>No se ha marcado el tipo de envase</p>";
-    $check = false;    
-} else {
+    if (($_REQUEST["marcaproducto"]) == "") {
+        print "<p>Falta la marca del producto</p>";
+    }
+
+    if (($_REQUEST["advertencia"]) == "") {
+        print "<p>No se ha indicado si el producto lleva o no advertencia por el consumo</p>";
+    }
+
+    if (($_REQUEST["fechaconsumo"]) == "") {
+        print "<p>No se ha indicado fecha de consumo preferente.</p>";
+    }
+
+    if (!isset($_REQUEST["alergenos"])) {
+        print "<p>No se ha indicado trazas de alergenos.</p>";
+    }
+    break;
+}
+
+while (
+    isset($_REQUEST["tipoenvase"]) &&
+    $_REQUEST["marcaproducto"] !== "" &&
+    $_REQUEST["advertencia"] !== "" &&
+    $_REQUEST["fechaconsumo"] !== "" &&
+    isset($_REQUEST["alergenos"])
+) {
+    $tipoCerveza = $_REQUEST["tipocerveza"];
+    $denominacionAlimento = $_REQUEST["denominacionalimento"];
     $tipoEnvase = $_REQUEST["tipoenvase"];
-    $check = true;
-}
-
-if (($_REQUEST["marcaproducto"]) == "") {
-    print "<p>Falta la marca del producto</p>";
-    $check = false;    
-} else {
+    $cantidadNeta = $_REQUEST["cantidadneta"];
     $marcaProducto = $_REQUEST["marcaproducto"];
-    $check = true;
-}
+    $advertencia = $_REQUEST["advertencia"];
+    $fechaConsumo = $_REQUEST["fechaconsumo"];
+    $alergenos = $_REQUEST["alergenos"];
+    $observaciones = $_REQUEST["observaciones"];
 
-if (($_REQUEST["advertencia"]) == "") {
-    print "<p>No se ha indicado si el producto lleva o no advertencia por el consumo</p>";
-    $check = false;    
-} else {
-    $marcaProducto = $_REQUEST["advertencia"];
-    $check = true;
-}
-
-
-print_r( $_REQUEST);
-print "<br>";
-
-while ($check == true) {
-    print "<br>se ha entrado";
+    print "<h2><span class=" . "exito" . ">EXITO</span> - Cerveza registrada correctamente.</h2>";
+    print "<p>Tipo de cerveza: <span class=" . "nobold" . ">$tipoCerveza</p>";
+    print "<p>Denominación del alimento: <span class=" . "nobold" . ">$denominacionAlimento</p>";
+    print "<p>Tipo de envase: <span class=" . "nobold" . ">$tipoEnvase</p>";
+    print "<p>Cantidad Neta: <span class=" . "nobold" . ">$cantidadNeta</p>";
+    print "<p>Marca del producto: <span class=" . "nobold" . ">$marcaProducto</p>";
+    print "<p>Advertencia sobre el abuso: <span class=" . "nobold" . ">$advertencia</p>";
+    print "<p>Fecha de consumo: <span class=" . "nobold" . ">$fechaConsumo</p>";
+    print "<p>Alergenos: <span class=" . "nobold" . ">$alergenos</p>";
+    print "<p>Observaciones: <span class=" . "nobold" . ">$observaciones</p>";
     break;
 }
 
 /*
 
-$tipoCerveza = $_REQUEST["tipocerveza"];
-$denominacionAlimento = $_REQUEST["denominacionalimento"];
-//$tipoEnvase = $_REQUEST["tipoenvase"];
-$cantidadNeta = $_REQUEST["cantidadneta"];
-$marcaProducto = $_REQUEST["marcaproducto"];
-$advertencia = $_REQUEST["advertencia"];
-$fechaConsumo = $_REQUEST["fechaconsumo"];
-$alergenos = $_REQUEST["alergenos"];
-$observaciones = $_REQUEST["observaciones"];
+
 
 */
 
-print "<br>Tipo de cerveza".$tipoCerveza;
+
 
 ?>
---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema2 - Principios de JavaScript/Ejercicios/05 Arrays y Objetos/Ejercicio16/js/main.js
@@ -0,0 +1,60 @@
+/*
+EJERCICIO16. 
+Tienes un array de objetos que representan productos en una tienda. 
+Cada objeto tiene un nombre, un precio, y una propiedad categoria, que es otro objeto con un nombre y una descripción. 
+
+Realiza las siguientes tareas:
+
+    1. Crear un array de productos donde cada producto tiene una categoría con nombre y descripción.
+    2. Obtener un array de nombres de productos pertenecientes a una categoría específica (por ejemplo,
+    "Electrónica"). Imprimirlo
+    3. Imprimir los detalles de los productos filtrados, incluyendo el nombre de la categoría y su
+    descripción.
+*/
+
+//1. Crear un array de productos donde cada producto tiene una categoría con nombre y descripción.
+let arrayProductos = [
+  {
+    nombre: "Producto1",
+    precio: 9.99,
+    categoria: { nombre: "Categoría1", descripcion: "Descripción1" },
+  },
+  {
+    nombre: "Producto2",
+    precio: 5.52,
+    categoria: { nombre: "Electrónica", descripcion: "Descripción2" },
+  },
+  {
+    nombre: "Producto3",
+    precio: 3.99,
+    categoria: { nombre: "Categoría3", descripcion: "Descripción3" },
+  },
+  {
+    nombre: "Producto4",
+    precio: 25.12,
+    categoria: { nombre: "Electrónica", descripcion: "Descripción4" },
+  },
+  {
+    nombre: "Producto5",
+    precio: 16,
+    categoria: { nombre: "Categoría5", descripcion: "Descripción5" },
+  },
+];
+
+//2. Obtener un array de nombres de productos pertenecientes a una categoría específica (por ejemplo,"Electrónica"). Imprimirlo
+function obtenerProductosCategoriaElectronica(parametro) {
+
+    let arrayProductosCategoriaElectronica = parametro.filter(element => element.categoria.nombre === "Electrónica");
+    return arrayProductosCategoriaElectronica;
+    
+}
+
+obtenerProductosCategoriaElectronica(arrayProductos).forEach((element) => {
+    console.log(`${element.nombre} ${element.categoria.nombre}`);
+});
+
+//3. Imprimir los detalles de los productos filtrados, incluyendo el nombre de la categoría y su descripción.
+
+obtenerProductosCategoriaElectronica(arrayProductos).forEach((element) => {
+    console.log(`${element.nombre} ${element.categoria.nombre} ${element.categoria.descripcion}`);
+});
