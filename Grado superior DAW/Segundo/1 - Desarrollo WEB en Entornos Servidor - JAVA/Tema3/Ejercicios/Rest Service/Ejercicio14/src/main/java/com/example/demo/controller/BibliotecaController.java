095b1b4f36a69f408ad3718ec2bf1348d4f744f4
juangabrielsv <juangabrielsv@gmail.com>
2024-10-25 15:48:18 +0200

    .

--- Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema3/Ejercicios/Rest Service/Ejercicio14/src/main/java/com/example/demo/controller/BibliotecaController.java
+++ Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema3/Ejercicios/Rest Service/Ejercicio14/src/main/java/com/example/demo/controller/BibliotecaController.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.modelo.Actor;
 import com.example.demo.modelo.Pelicula;
 
 @RestController
+@RequestMapping("biblioteca")
 public class BibliotecaController {
-	
-	private List<Pelicula> listaPeliculas;
-
-	
-	
+		
+	private List<Pelicula> listaPeliculas;	
 
 	public BibliotecaController() {
+		
 		listaPeliculas = new ArrayList<>();
 		
 		List<Actor> listaActores1 = new ArrayList<>();
