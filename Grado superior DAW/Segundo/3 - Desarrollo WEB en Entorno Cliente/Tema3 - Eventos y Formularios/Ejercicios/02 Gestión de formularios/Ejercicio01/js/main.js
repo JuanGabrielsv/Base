b91778109b426572393845db5d6c9b7e80c6c488
Juangabrielsv <juangabrielsv@gmail.com>
2024-11-03 10:10:36 +0100

    .

---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema3 - Eventos y Formularios/Ejercicios/02 Gestión de formularios/Ejercicio01/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let aFormulario = document.forms['formulario'];
+
+    console.log(aFormulario);
+    console.log(document.forms['formulario']);
+});
+
