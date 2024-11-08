08714aa452265c75bd1cab0fa1c019d2ea418966
Juangabrielsv <juangabrielsv@gmail.com>
2024-11-08 20:10:19 +0100

    .

---
+++ Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema4/Ejercicios/Ejercicio01/src/main/java/com/example/demo/modelo/Cliente.java
@@ -0,0 +1,57 @@
+package com.example.demo.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "nombre")
+	private String nombre;
+	@Column(name = "apellidos")
+	private String apellidos;
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(String nombre, String apellidos) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+}
