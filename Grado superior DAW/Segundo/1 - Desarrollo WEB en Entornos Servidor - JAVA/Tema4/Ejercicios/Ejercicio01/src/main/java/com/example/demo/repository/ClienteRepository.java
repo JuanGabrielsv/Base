d2446d14a95bde1dca10337b519f92a93277d0f0
Juangabrielsv <juangabrielsv@gmail.com>
2024-11-09 12:31:33 +0100

    .

--- Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema4/Ejercicios/Ejercicio01/src/main/java/com/example/demo/repository/ClienteRepository.java
+++ Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema4/Ejercicios/Ejercicio01/src/main/java/com/example/demo/repository/ClienteRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
-
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import com.example.demo.modelo.Cliente;
@@ -18,7 +17,6 @@ public class ClienteRepository {
 		Query<Cliente> query = (Query<Cliente>) entityManager.createQuery("select c from Cliente c", Cliente.class);
 		List<Cliente> listaCliente = query.getResultList();
 		return listaCliente;
-	}
-	
+	}	
 	
 }
