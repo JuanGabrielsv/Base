731da2ff7b847dff89a5b7f76ef83144c74655b9
juangabrielsv <juangabrielsv@gmail.com>
2024-11-11 20:30:23 +0100

    .

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema4 - DOM en Javascript/Ejercicios/01 DOM en JavaScript/Ejercicio12/src/js/main.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema4 - DOM en Javascript/Ejercicios/01 DOM en JavaScript/Ejercicio12/src/js/main.js
@@ -1,5 +1,13 @@
 /*
+EJERCICIO12
 
+• Crear  una página HTML con 3 párrafos con <p> con los siguientes id: ‘contenidos_1’, ‘contenidos_2’ y
+  ‘contenidos_3’. 
+• Detrás de cada párrafo, poner un enlace <a> con el texto ‘Ocultar Contenido’ y con sus id:
+  ‘enlace_1’, ‘enlace_2’ y ‘enlace_3’.
+• Realizar un programa que desde código JavaScript, al pulsar un enlace se ocultará el texto del párrafo
+  correspondiente. Cuando se oculte, el texto del enlace deberá mostrar ‘Mostrar Contenido’ y al pulsarlo, se
+  visualizará el párrafo volviendo a mostrar el enlace el texto ‘ocultar contenido’.
 */
 
 window.addEventListener('DOMContentLoaded', () => {
