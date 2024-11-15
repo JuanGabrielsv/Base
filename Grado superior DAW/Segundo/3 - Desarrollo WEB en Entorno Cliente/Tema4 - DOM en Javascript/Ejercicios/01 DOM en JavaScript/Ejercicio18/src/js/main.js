411a8fd3cd517338a7b5b87c6a016d4f7bd9e1fe
Juangabrielsv <juangabrielsv@gmail.com>
2024-11-15 14:25:10 +0100

    .

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema4 - DOM en Javascript/Ejercicios/01 DOM en JavaScript/Ejercicio18/src/js/main.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema4 - DOM en Javascript/Ejercicios/01 DOM en JavaScript/Ejercicio18/src/js/main.js
@@ -1,24 +1,8 @@
 /*
-EJERCICIO17.
+EJERCICIO18.
 
-• A partir del siguiente código HTML:
-
-    <h1>Tabla HTML dibujada con JS</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Precio</th>
-                    <th>Código</th>
-                </tr>
-            </thead>
-            <tbody id="cuerpoTabla"></tbody>
-        </table>
-
-• Crear desde javascript las filas (tr) y columnas (td) de dicha tabla dentro del cuerpo de la tabla (tbody) de la siguiente forma: 
-• Crear un array de 4 elementos, donde cada elemento será un objeto literal con los campos: id, nombre, precio y código. 
-• Cada objeto literal tendrá los datos de una fila. 
-• El id del objeto tendrá que asignárselo al id de la fila (<tr>).
+• Sobre el ejercicio anterior, cuando pulsemos el botón, añadir desde Javascript una columna nueva, que será un checkbox. 
+• Cada checkbox tiene un id diferente: “checkbox1”, “checkbox2”,… y un name con el valor “marcar.”
 */
 
 window.addEventListener('DOMContentLoaded', () => {
