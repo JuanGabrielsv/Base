3168eb6a98a5c22fef20b0240521eac6708aff62
juangabrielsv <juangabrielsv@gmail.com>
2024-12-03 17:47:53 +0100

    .

--- Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema4/Ejercicios/Ejercicio01/src/main/java/com/example/demo/repository/ClienteRepositoryImpl.java
+++ Grado superior DAW/Segundo/1 - Desarrollo WEB en Entornos Servidor - JAVA/Tema4/Ejercicios/Ejercicio01/src/main/java/com/example/demo/repository/ClienteRepositoryImpl.java
@@ -21,22 +21,18 @@ public class ClienteRepositoryImpl {
 	}
 
 	public Cliente getCliente(Integer id) {
-
 		Cliente cliente = entityManager.find(Cliente.class, id);
 		return cliente;
 	}
 
 	public Cliente insertaCliente(Cliente cliente) {
-
 		// es necesario @transaccional en el servicio
 		entityManager.persist(cliente);
 		System.out.println(cliente);
 		return cliente;
-
 	}
 
 	public Boolean deleteCliente(Integer id) {
-
 		// necesario @transaccional en el servicio.
 		Cliente c = getCliente(id);
 		if (c != null) {
@@ -44,29 +40,21 @@ public class ClienteRepositoryImpl {
 			return true;
 		}
 		return false;
-
 	}
 
 	public Cliente actualizarCliente(Cliente cliente) {
-
 		return entityManager.merge(cliente);
-
 	}
 
 	public List<Cliente> getClientesNombre(String nombre) {
-
 		// Crear la consulta JPQL
 		String jpql = "SELECT c FROM Cliente c WHERE c.nombre LIKE :nombre";
-
 		// Crear la consulta
 		Query<Cliente> query = (Query<Cliente>) entityManager.createQuery(jpql, Cliente.class);
-
 		// Configurar el parÃ¡metro
 		query.setParameter("nombre", "%" + nombre + "%");
-
 		// Ejecutar la consulta y obtener la lista de clientes
 		return query.getResultList();
-
 	}
 
 }
