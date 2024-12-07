1fafe5e57217d2324febf8ddbb64dc05a27be13c
Juangabrielsv <juangabrielsv@gmail.com>
2024-12-07 19:43:05 +0100

    Terminado ejercicio01 del tema5 Ajax del módulo desarrollo web entorno cliente javaScript

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema5 - Ajax/Ejercicios/01 Ajax/Ejercicio 01/js/index.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema5 - Ajax/Ejercicios/01 Ajax/Ejercicio 01/js/index.js
@@ -5,19 +5,42 @@ Obtener el usuario con id=5 de jsonplaceholder.typicode.com/users.
 */
 
 iniciarPrograma = () => {
-    const xhr = new XMLHttpRequest(); // Creamos la instancia de XMLHttpRequest    
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/users/5", true); // Configuramos la solicitud de petición    
-    xhr.send(); // Enviamos la solicitud de petición previamente configurada    
-    xhr.addEventListener("readystatechange", (e) => { // Configurar la función de devolución de llamada cuando la solicitud se complete
-        if (xhr.readyStatete !== 4) {
+
+    // Creamos la instancia de XMLHttpRequest.
+    const xhr = new XMLHttpRequest();
+
+    // Configuramos la solicitud de petición.
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users/5", true);
+
+    // Configurar la función de devolución de llamada cuando la solicitud se complete.
+    xhr.addEventListener("readystatechange", (e) => {
+        if (xhr.readyState !== 4) {
             return;
         }
         if (xhr.status >= 200 && xhr.status < 300) { // La solicitud fué exitosa.
-            let json = JSON.parse(xhr.responseText); // Se parsea a formato json los datos obtenidos.
-            console.log(json.name); // Imprime por consola el contenido del json que le indiquemos.
+            let usuario = JSON.parse(xhr.responseText); // Se parsea a formato json los datos obtenidos.
+            console.log(usuario);
+            mostrarInfoUsuario(usuario);
         } else { // La solicitud falló.
             document.getElementById("usuario-info").innerHTML = "Error al cargar el usuario";
-            let message = xhr.statusText || "Ocurrió un error";
         }
     });
+
+    // Configurar la función de devolución de llamada para manejar errores de red.
+    xhr.onerror = function () {
+        document.getElementById("usuario-info").innerHTML = "Error de red";
+    };
+
+    // Enviamos la solicitud.
+    xhr.send();
+};
+
+mostrarInfoUsuario = (usuario) => {
+    let usuarioInfoDiv = document.getElementById("usuario-info");
+    usuarioInfoDiv.innerHTML = '<h2>Nombre: ' + usuario.name + '</h2>';
+    usuarioInfoDiv.innerHTML += '<p>Usuario: ' + usuario.username + '</p>';
+    usuarioInfoDiv.innerHTML += '<p>Email: ' + usuario.email + '</p>';
+    usuarioInfoDiv.innerHTML += '<p>Dirección: ' + usuario.address.street + ', ' + usuario.address.suite + ', ' + usuario.address.city + '</p>';
 }
+
+window.addEventListener('DOMContentLoaded', iniciarPrograma);
