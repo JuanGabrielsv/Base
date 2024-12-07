de5bcadcaa23f4f91c155dd7090acf4e1ec80772
Juangabrielsv <juangabrielsv@gmail.com>
2024-12-07 18:54:54 +0100

    Realizando ejercicio tema 5 ajax en javaScript

--- Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema5 - Ajax/Ejercicios/01 Ajax/Ejercicio 01/js/index.js
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema5 - Ajax/Ejercicios/01 Ajax/Ejercicio 01/js/index.js
@@ -1,29 +1,23 @@
-/* 1.- Obtener el usuario con id=5 de jsonplaceholder.typicode.com/users.
-Mostrar los datos del usuario en distintos <p> de un div de una página html: Nombre, usuario, correo y la
-dirección. */
+/* EJERCICIO01 
 
-const xhr = new XMLHttpRequest();
+Obtener el usuario con id=5 de jsonplaceholder.typicode.com/users.
+• Mostrar los datos del usuario en distintos <p> de un div de una página html: Nombre, usuario, correo y la dirección.
+*/
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/users/5", true);
-xhr.send();
-xhr.addEventListener("readystatechange", (e) => {
-    if (xhr.readyState !== 4) return;
-
-    if (xhr.status >= 200 && xhr.status < 300) {
-        console.log("éxito");
-
-        let json = JSON.parse(xhr.responseText);
-        console.log(json.name);
-
-
-
-
-    } else {
-        console.log('error');
-        let message = xhr.statusText || "Ocurrió un error";
-        //$xhr.innerHTML = `Error ${xhr.status}: ${message}`;
-    }
-
-
-
-})
+iniciarPrograma = () => {
+    const xhr = new XMLHttpRequest(); // Creamos la instancia de XMLHttpRequest    
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users/5", true); // Configuramos la solicitud de petición    
+    xhr.send(); // Enviamos la solicitud de petición previamente configurada    
+    xhr.addEventListener("readystatechange", (e) => { // Configurar la función de devolución de llamada cuando la solicitud se complete
+        if (xhr.readyStatete !== 4) {
+            return;
+        }
+        if (xhr.status >= 200 && xhr.status < 300) { // La solicitud fué exitosa.
+            let json = JSON.parse(xhr.responseText); // Se parsea a formato json los datos obtenidos.
+            console.log(json.name); // Imprime por consola el contenido del json que le indiquemos.
+        } else { // La solicitud falló.
+            document.getElementById("usuario-info").innerHTML = "Error al cargar el usuario";
+            let message = xhr.statusText || "Ocurrió un error";
+        }
+    });
+}
