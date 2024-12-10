39b09847f1c3faf8464d0d0ff939f9792139d0e2
juangabrielsv <juangabrielsv@gmail.com>
2024-12-10 16:24:40 +0100

    .

---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema5 - Ajax/Ejercicios/01 Ajax/Ejercicio 03/js/main.js
@@ -0,0 +1,8 @@
+/*
+EJERCICIO 03.
+• Crear una página html con un <h1> ¿Sí o No?. 
+• Tendrá un botón y un <p> para mostrar ahí la respuesta.
+• Cuando pulsemos el botón, se conectará con la api yesno.wtf/api para obtener la respuesta Si o No.
+• Crear otra página html2 igual a la anterior.
+• Al pulsar el botón, cargaremos en el resultado la imagen que devuelva. */
+
