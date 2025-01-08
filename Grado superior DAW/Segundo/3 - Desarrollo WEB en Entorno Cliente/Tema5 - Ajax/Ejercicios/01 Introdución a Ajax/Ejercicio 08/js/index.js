16be4711c76430281956eedc200671ddb7e23815
Juangabrielsv <juangabrielsv@gmail.com>
2025-01-08 13:16:04 +0100

    Realizando ejercicio 08 - Desarrollo Web entorno Cliente - Tema5 Ajax.

---
+++ Grado superior DAW/Segundo/3 - Desarrollo WEB en Entorno Cliente/Tema5 - Ajax/Ejercicios/01 Introdución a Ajax/Ejercicio 08/js/index.js
@@ -0,0 +1,75 @@
+/*
+EJERCICIO 08.
+Queremos obtener datos aleatorios de 10 usuarios: https://randomuser.me/api/?results=10.
+1. Mostraremos la foto y su nombre, apellidos, correo, dirección y ciudad.
+2. En html tenemos una capa div. 
+3. Para cada usuario tendremos que crear un div desde js, y unirlo al div de la página html.
+4. Cada usuario, tendrá un botón para poder cambiar aleatorioamente ese usuario.
+5. Hacerlo con fetch y promesas.
+*/
+iniciar = () => {
+    const capaDiv = document.getElementById('capaDiv')
+    capaDiv.style = 'display: grid; grid-template-columns: 1fr 1fr 1fr 1fr 1fr;'
+    let idUsuario = 1
+
+    fetch('https://randomuser.me/api/?results=10')
+        .then((response) => response.json())
+        .then((data) =>
+            data.results.forEach((element) => {
+
+                if (idUsuario > 10) {
+                    idUsuario = 1
+                }
+
+                let boton = document.createElement('button')
+                boton.textContent = 'Cambiar'
+                boton.id = `usuario${idUsuario}`
+
+                let divUsuario = document.createElement('div')
+                divUsuario.style = 'text-align: center; margin-top: 2rem'
+                divUsuario.id = `id-usuario${idUsuario}`
+                let imagen = document.createElement('img')
+                imagen.src = `${element.picture.large}`
+                let parrafo = document.createElement('p')
+
+
+                parrafo.innerHTML = `${element.name.title} 
+                ${element.name.first} 
+                ${element.name.last} <br> 
+                ${element.email} <br>
+                ${element.location.street.name},
+                ${element.location.street.number} <br>
+                ${element.location.city} ${element.location.country}`
+
+                divUsuario.appendChild(imagen)
+                divUsuario.appendChild(parrafo)
+                divUsuario.appendChild(boton)
+                capaDiv.appendChild(divUsuario)
+
+                idUsuario++
+
+                console.log(element)
+
+                boton.addEventListener('click', () => {
+                    fetch('https://randomuser.me/api/?results=1')
+                        .then((response) => response.json())
+                        .then((data) => data.results.forEach((element) => {
+
+                            let cambiarUsuario = document.getElementById(`id-${boton.id}`)
+                            console.log(cambiarUsuario)
+
+                        }))
+                        .catch((error) => console.log('Error:', error))
+
+                    console.log(boton.id)
+                })
+
+
+            })
+        )
+        .catch((error) => console.log('Error:', error))
+
+
+}
+
+window.addEventListener('DOMContentLoaded', iniciar)
