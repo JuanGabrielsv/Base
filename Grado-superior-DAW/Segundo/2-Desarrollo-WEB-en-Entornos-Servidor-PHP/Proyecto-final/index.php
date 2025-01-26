7959c7e3e7ca81ab79e092576267170b114df0cb
Juangabrielsv <juangabrielsv@gmail.com>
2025-01-26 22:34:15 +0100

    página login realizada en proyecto final php

---
+++ Grado-superior-DAW/Segundo/2-Desarrollo-WEB-en-Entornos-Servidor-PHP/Proyecto-final/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Aplicación para comprar cervezas, menos cruzcampo...">
+    <link rel="shortcut icon" href="src/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="src/css/index.css">
+    <title>Cervecera - Aquí no vendemos cruzcampo</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <img src="src/img/logotipo.svg" alt="logotipo de una jarra de cerveza">
+            <h1>APP - Aquí no vendemos cruzcampo.</h1>
+        </div>
+    </header>
+    <main>
+        <form action="" method="post">
+            <label for="nombre-usuario">Nombre usuario</label>
+            <input type="text" id="nombre-usuario" name="nombre-usuario" required>
+            <label for="contrasena">Contraseña</label>
+            <input type="password" id="contrasena" name="contrasena" required>
+            <button type="submit">Iniciar sesión</button>
+        </form>
+    </main>
+    <footer>
+        <p>Aplicación cervecería "Aquí no vendemos cruzcampo" - jsanches8979 &#174 2025 </p>
+    </footer>
+</body>
+
+</html>
