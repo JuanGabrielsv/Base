aa00ce72b6100f52de7ab15b4c47559fad1fbab6
Juangabrielsv <juangabrielsv@gmail.com>
2025-01-27 23:34:18 +0100

    Avanzando en los ejercicio Angular

--- Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Ejercicio03/src/app/alumno/alumno.component.ts
+++ Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Ejercicio03/src/app/alumno/alumno.component.ts
@@ -5,17 +5,35 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './alumno.component.html',
-  styleUrl: './alumno.component.css'
+  styleUrl: './alumno.component.css',
 })
 export class AlumnoComponent {
-	/* Mostrar un título (h1): ‘Componente Alumno’ de color rojo */
-	textoRojo: string = 'textoRojo';
+  /* Mostrar un título (h1): ‘Componente Alumno’ de color rojo */
+  textoRojo: string = 'textoRojo';
 
-	/* Que tenga las siguientes propiedades: Nombre, apellido, edad y dni. Dar valores y mostrarlos en
+  /* Que tenga las siguientes propiedades: Nombre, apellido, edad y dni. Dar valores y mostrarlos en
 	la vista o plantilla (html) con etiquetas <p>. */
-	nombre: string = 'Juan Gabriel';
-	apellido: string = 'Sánchez';
-	edad: number = 41;
-	dni: string = '53278134F';
+  nombre: string = 'Juan Gabriel';
+  apellido: string = 'Sánchez';
+  edad: number = 41;
+  dni: string = '53278134F';
 
+  /*¿Qué tengo que cambiar si quiero que el cuadro de texto estará deshabilitado(disabled)?. Usar
+	property binding. */
+  deshabilitado: boolean = true;
+
+  /*Crear en la vista otro checkbox que inicialmente esté desmarcado y cuando se marque, se
+	muestre un mensaje alert con un mensaje. Probad a cambiar el valor del booleano en el
+	componente.*/
+  checked: boolean = false;
+  texto: string = 'Antes de pulsar';
+
+  mostrarAlert() {
+    alert('Check marcado');
+  }
+
+  mostrarMensaje() {
+    alert('Check marcado');
+    this.texto = 'Check pulsado';
+  }
 }
