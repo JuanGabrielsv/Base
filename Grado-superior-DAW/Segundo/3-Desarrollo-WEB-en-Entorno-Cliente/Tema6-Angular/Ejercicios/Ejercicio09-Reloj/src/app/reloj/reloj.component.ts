c0809e236dfc42bf0bae90f94adc428328d59cd6
juangabrielsv <juangabrielsv@gmail.com>
2025-01-29 16:44:53 +0100

    Terminado ejercicio09 reloj Angular

--- Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Ejercicio09-Reloj/src/app/reloj/reloj.component.ts
+++ Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Ejercicio09-Reloj/src/app/reloj/reloj.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reloj',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './reloj.component.html',
-  styleUrl: './reloj.component.css'
+  styleUrl: './reloj.component.css',
 })
 export class RelojComponent {
+  hora: string = '12:00:00';
+  horaModificada: string = '';
+  horaActual: Date = new Date();
 
+  cambiarHora() {
+    this.horaModificada = this.hora;
+  }
+
+  temp = setInterval(() => {
+    this.horaActual = new Date();
+  }, 1000);
 }
