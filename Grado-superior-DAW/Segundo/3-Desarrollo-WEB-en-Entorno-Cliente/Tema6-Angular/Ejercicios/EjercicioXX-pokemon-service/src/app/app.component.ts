3b4a5104cf64fd15059e3294a0f88301e168c9fe
juangabrielsv <juangabrielsv@gmail.com>
2025-01-29 18:31:36 +0100

    Realizado ejercicio de servicio api en Angular

---
+++ Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/EjercicioXX-pokemon-service/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PokemonServiceService } from './services/pokemon-service.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  title = 'EjercicioXX-pokemon-service';
+
+  pokemonList = new Observable<any>();
+
+  constructor(private servicioPokemon: PokemonServiceService) {}
+
+  ngOnInit(): void {
+    this.pokemonList = this.servicioPokemon.getListaPokemon();
+    throw new Error('Method not implemented');
+  }
+}
