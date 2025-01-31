3d671c99a1b1709b18b35ca03857ef3be9047492
juangabrielsv <juangabrielsv@gmail.com>
2025-01-31 16:55:29 +0100

    Terminado Ejercicio17 Angular

--- Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Ejercicio17-Personas/src/app/app.component.ts
+++ Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Ejercicio17-Personas/src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListadoPersonasComponent } from './listado-personas/listado-personas.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListadoPersonasComponent],
+  imports: [RouterOutlet, ListadoPersonasComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Ejercicio17-Personas';
+  personas: Persona[] = [
+    {
+      nombre: 'Persona 1',
+      apellidos: 'Apellidos 1',
+      edad: 23,
+    },
+    {
+      nombre: 'Persona 2',
+      apellidos: 'Apellidos 2',
+      edad: 12,
+    },
+    {
+      nombre: 'Persona 3',
+      apellidos: 'Apellidos 3',
+      edad: 42,
+    },
+    {
+      nombre: 'Persona 4',
+      apellidos: 'Apellidos 4',
+      edad: 5,
+    },
+  ];
+}
+
+export interface Persona {
+  nombre: string;
+  apellidos: string;
+  edad: number;
 }
