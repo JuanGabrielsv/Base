f497ed2b19ccea01366693efdc16ea70261cccc1
Juangabrielsv <juangabrielsv@gmail.com>
2025-02-01 12:38:46 +0100

    Ejercicio01 servicios API Angular Pokemon

--- Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Servicios-API/EjercicioXX-pokemon-service/src/app/app.config.ts
+++ Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Servicios-API/EjercicioXX-pokemon-service/src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+//Paso 3: En app.config.td importamos provideHttpClient().
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
--- Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Servicios-API/EjercicioXX-pokemon-service/src/app/services/pokemon-service.service.ts
+++ Grado-superior-DAW/Segundo/3-Desarrollo-WEB-en-Entorno-Cliente/Tema6-Angular/Ejercicios/Servicios-API/EjercicioXX-pokemon-service/src/app/services/pokemon-service.service.ts
@@ -6,8 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class PokemonServiceService {
   private url = 'https://pokeapi.co/api/v2/pokemon';
+
+  // Paso 1: Creamos constructor e inyectamos el objeto HttpClient.
   constructor(private http: HttpClient) {}
 
+  // Paso 2: Creamos los métodos que queramos para llamar a this.http.get.
   getListaPokemon() {
     return this.http.get(this.url);
   }
