ffe744c1eb1cca371d2950bbb1a455195c520769
JuanGabrielsv <juangabrielsv@gmail.com>
2025-02-05 20:33:19 +0100

    vigilar

--- Grado-superior-DAW/Segundo/2-Desarrollo-WEB-en-Entornos-Servidor-PHP/Proyecto-final/src/php/formulario.php
+++ Grado-superior-DAW/Segundo/2-Desarrollo-WEB-en-Entornos-Servidor-PHP/Proyecto-final/src/php/formulario.php
@@ -16,48 +16,65 @@
 		</div>
 	</header>
 	<main>
-		<h2>Inserción de Cervezas</h2>
-		<p>Introduzca los datos de la Cerveza:</p>
-		<form action="registro.php" method="get">
-			<div>
-				<label for="denominacion">Denominación Cerveza:</label>
-				<br>
-				<label for="marca">Marca:</label>
-				<br>
-				<label for="tipo">Tipo de cerveza:</label>
-				<br>
-				<button type="submit">Guardar</button>
-			</div>
-			<div>
-				<input type="text" name="denominacion" id="denominacion" required>
-				<br>
-				<select name="marca" id="marca">
-					<option value="Heineken">Heineken</option>
-					<option value="Mahou">Mahou</option>
-					<option value="Dam">Dam</option>
-					<option value="Estrella-Galicia">Estrella Galicia</option>
-					<option value="Alhambra">Alhambra</option>
-					<option value="Cruzcampo">Cruzcampo</option>
-					<option value="Artesana">Artesana</option>
-				</select>
-				<br>
-				<input type="radio" name="tipo" id="tipo" value="lager">
-				<label for="tipo">Lager</label>
-				<input type="radio" name="tipo" id="tipo" value="Pale ale">
-				<label for="tipo">Pale ale</label>
-				<input type="radio" name="tipo" id="tipo" value="Cerveza-negra">
-				<label for="tipo">Cerveza Negra</label>
-				<input type="radio" name="tipo" id="tipo" value="Abadia">
-				<label for="tipo">Abadia</label>
-				<input type="radio" name="tipo" id="tipo" value="Rubia">
-				<label for="tipo">Rubia</label>
-			</div>
-
-		</form>
-
+		<section class="section1">
+			<h2>Inserción de Cervezas</h2>
+			<p>Introduzca los datos de la Cerveza</p>
+			<form action="registro.php" method="get" class="section-form">
+				<div class="form-fila">
+					<label for="denominacion">Denominación cerveza:</label>
+					<input type="tel" name="denominacion" id="denominacion">
+				</div>
+				<div class="form-fila">
+					<label for="marca">Marca:</label>
+					<select name="marca" id="marca" class="form-select">
+						<option value="alhambra" selected>Alhambra</option>
+						<option value="heineken">Heineken</option>
+						<option value="mahou">Mahou</option>
+						<option value="dam">Dam</option>
+						<option value="estrella Galicia">Estrella Galicia</option>
+						<option value="cruzcampo">Cruzcampo</option>
+						<option value="artesana">Artesana</option>
+					</select>
+				</div>
+				<div class="form-fila">
+					<label for="tipo">Tipo de cerveza:</label>
+					<div>
+						<input type="checkbox" name="tipo" id="tipo" value="lager">
+						<label for="tipo">Lager</label>
+						<input type="checkbox" name="tipo" id="tipo" value="Pale Ale">
+						<label for="tipo">Pale Ale</label>
+						<input type="checkbox" name="tipo" id="tipo" value="Cerveza Negra">
+						<label for="tipo">Cerveza Negra</label>
+						<input type="checkbox" name="tipo" id="tipo" value="Abadia">
+						<label for="tipo">Abadia</label>
+						<input type="checkbox" name="tipo" id="tipo" value="Rubia">
+						<label for="tipo">Rubia</label>
+					</div>
+				</div>
+				<div class="form-fila">
+					<label for="formato">Formato:</label>
+					<select name="formato" id="formato" class="form-select">
+						<option value="lata" selected>Lata</option>
+						<option value="botella" selected>Botella</option>
+						<option value="pack" selected>Pack</option>
+					</select>
+				</div>
+				<div class="form-fila">
+					<label for="tamano">Tamaño:</label>
+					<select name="tamano" id="tamano" class="form-select">
+						<option value="botellin">Botellín</option>
+						<option value="tercio">Tercio</option>
+						<option value="media">Media</option>
+						<option value="litrona" selected>Litrona</option>
+						<option value="pack">Pack</option>
+					</select>
+				</div>
+				<button>Insertar Cerveza</button>
+			</form>
+		</section>
 	</main>
 	<footer>
-		<p>Aplicación cervecería "Aquí no vendemos cruzcampo" - jsanches8979 &#174 2025 </p>
+		<p>Aplicación cervecería "Aquí no vendemos cruzcampo" - jsanchez8979 &#174 2025 </p>
 	</footer>
 </body>
 
