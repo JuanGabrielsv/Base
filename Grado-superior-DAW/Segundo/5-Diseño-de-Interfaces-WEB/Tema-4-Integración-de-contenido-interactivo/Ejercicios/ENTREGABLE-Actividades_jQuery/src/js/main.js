04fee52ae7f467315c1b92de92712c67d091a595
Juangabrielsv <juangabrielsv@gmail.com>
2025-02-06 10:32:27 +0100

    cuidadín

--- Grado-superior-DAW/Segundo/5-Diseño-de-Interfaces-WEB/Tema-4-Integración-de-contenido-interactivo/Ejercicios/ENTREGABLE-Actividades_jQuery/src/js/main.js
+++ Grado-superior-DAW/Segundo/5-Diseño-de-Interfaces-WEB/Tema-4-Integración-de-contenido-interactivo/Ejercicios/ENTREGABLE-Actividades_jQuery/src/js/main.js
@@ -1,7 +1,65 @@
 $(function () {
-    $(iniciarSesion).click(function (e) {
-        e.preventDefault()
-        $(section1).fadeOut()
-        $('h1').fadeOut()
-    })
+	$(section2).hide()
+	$(section3).hide()
+	$(section4).hide()
+	$(section5).hide()
+
+	$('#iniciarSesion').click(function (e) {
+		e.preventDefault()
+		$('#section1').fadeOut(function () {
+			$('#section2').fadeIn()
+		})
+		$('h1').fadeOut(function () {
+			$('h1').text('Cuenta').fadeIn()
+		})
+		if (
+			$('#email').val().trim() === '' ||
+			$('#password').val().trim() === ''
+		) {
+			$('#iniciarSesion2').prop('disabled', true)
+		} else {
+			$('#iniciarSesion2').prop('disabled', false)
+		}
+
+		$('#email, #password').on('input', function () {
+			if (
+				$('#email').val().trim() === '' ||
+				$('#password').val().trim() === ''
+			) {
+				$('#iniciarSesion2').prop('disabled', true)
+			} else {
+				$('#iniciarSesion2').prop('disabled', false)
+			}
+		})
+	})
+
+	$('#crearCuenta').click(function (e) {
+		e.preventDefault()
+		$('#section1').fadeOut(function () {
+			$('#section3').fadeIn()
+		})
+		$('h1').fadeOut(function () {
+			$('h1').text('Cuenta').fadeIn()
+		})
+	})
+
+	$(proveedor).click(function (e) {
+		e.preventDefault()
+		$(section3).fadeOut(function () {
+			$(section4).fadeIn()
+		})
+		$('h1').fadeOut(function () {
+			$('h1').text('Crear una cuenta Proveedor').fadeIn()
+		})
+	})
+
+	$(clienteFinal).click(function (e) {
+		e.preventDefault()
+		$(section3).fadeOut(function () {
+			$(section5).fadeIn()
+		})
+		$('h1').fadeOut(function () {
+			$('h1').text('Crear una cuenta Cliente').fadeIn()
+		})
+	})
 })
