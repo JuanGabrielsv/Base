2f4ff9ba9ebd61bb8a2fcf765ce8712a93ef12cb
Juangabrielsv <juangabrielsv@gmail.com>
2025-02-17 11:58:33 +0100

    .

--- Grado-superior-DAW/Segundo/5-Diseño-de-Interfaces-WEB/Tema-6-Desarrollo-de-interfaces-web-amigables/Ejercicio/Protección-de-datos/src/script/main.js
+++ Grado-superior-DAW/Segundo/5-Diseño-de-Interfaces-WEB/Tema-6-Desarrollo-de-interfaces-web-amigables/Ejercicio/Protección-de-datos/src/script/main.js
@@ -2,6 +2,10 @@ window.addEventListener('DOMContentLoaded', () => {
 	const boton = document.getElementById('botonAceptar')
 	const bloque = document.getElementById('bloqueCookie')
 
+	if (sessionStorage.getItem('cookie')) {
+		bloque.style.display = 'none'
+	}
+
 	boton.addEventListener('click', () => {
 		bloque.style.display = 'none'
 		sessionStorage.setItem('cookie', 1)
